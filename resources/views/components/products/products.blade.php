45122cdd64f38153c7abf02c8a0a5ff1d5d76842
mehrangit <mehran.mj9999@gmail.com>
2024-02-24 10:28:36 +0330

    component type 4 , 5 , 6 finished.

--- resources/views/components/products/products.blade.php
+++ resources/views/components/products/products.blade.php
@@ -1,6 +1,6 @@
 @props(['type' => '1'])
 {{-- <div class="grid grid-cols-1 gap-4 md:grid-cols-2 {{ $type === '1' || $type === '2' ? 'lg:grid-cols-5' : $type === '3' : 'lg:grid-cols-3' : 'lg:grid-cols-3' }}"> --}}
-<div class="grid grid-cols-1 gap-4 md:grid-cols-2 {{ $type === '1' ||  $type === '2' ? 'lg:grid-cols-5' : ($type === '3' ? 'lg:grid-cols-3' : ($type === '4' ? 'lg:grid-cols-2' : 'lg:grid-cols-8')) }}">
+<div class="grid grid-cols-1 gap-4 md:grid-cols-2 {{ $type === '1' ||  $type === '2' ? 'lg:grid-cols-5' : ($type === '3' ? 'lg:grid-cols-3' : ($type === '4' || $type === '5' ? 'lg:grid-cols-2' : ($type === '6' ? 'lg:grid-cols-2 md:gap-0' : 'lg:grid-cols-8'))) }}">
     <x-products.product :type="$type" image="{{ asset('assets/images/test/small-truck.png') }}" href="#" />
     <x-products.product :type="$type" image="{{ asset('assets/images/test/small-truck.png') }}" href="#" />
     <x-products.product :type="$type" image="{{ asset('assets/images/test/small-truck.png') }}" href="#" />
