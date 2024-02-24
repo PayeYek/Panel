0d8b8a1a0ec5ad6955a9ee80e9581c30c3f7a64a
mehrangit <mehran.mj9999@gmail.com>
2024-02-24 11:15:00 +0330

    type 7 finished

--- resources/views/components/products/products.blade.php
+++ resources/views/components/products/products.blade.php
@@ -1,6 +1,6 @@
 @props(['type' => '1'])
 {{-- <div class="grid grid-cols-1 gap-4 md:grid-cols-2 {{ $type === '1' || $type === '2' ? 'lg:grid-cols-5' : $type === '3' : 'lg:grid-cols-3' : 'lg:grid-cols-3' }}"> --}}
-<div class="grid grid-cols-1 gap-4 md:grid-cols-2 {{ $type === '1' ||  $type === '2' ? 'lg:grid-cols-5' : ($type === '3' ? 'lg:grid-cols-3' : ($type === '4' || $type === '5' ? 'lg:grid-cols-2' : ($type === '6' ? 'lg:grid-cols-2 md:gap-0' : 'lg:grid-cols-8'))) }}">
+<div class="grid grid-cols-1 {{ $type === '1' ||  $type === '2' ? 'lg:grid-cols-5 gap-4 md:grid-cols-2' : ($type === '3' ? 'lg:grid-cols-3 gap-4 md:grid-cols-2' : ($type === '4' || $type === '5' ? 'md:grid-cols-2 lg:grid-cols-2 gap-4' : ($type === '6' ? 'md:grid-cols-2 lg:grid-cols-2 gap-4 md:gap-0' : ($type === '7' ? 'md:grid-cols-1 rounded-xl overflow-hidden gap-0' : 'md:grid-cols-2 gap-4 lg:grid-cols-8')))) }}">
     <x-products.product :type="$type" image="{{ asset('assets/images/test/small-truck.png') }}" href="#" />
     <x-products.product :type="$type" image="{{ asset('assets/images/test/small-truck.png') }}" href="#" />
     <x-products.product :type="$type" image="{{ asset('assets/images/test/small-truck.png') }}" href="#" />
