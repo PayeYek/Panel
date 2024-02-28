5a1ad9b849be41dd9aedc5f725497d67e6d21029
themestermim <themestermim@gmail.com>
2024-02-28 04:28:33 -0800

    some bugs fixed.

--- resources/views/components/home_landing/announcement/index.blade.php
+++ resources/views/components/home_landing/announcement/index.blade.php
@@ -5,6 +5,16 @@
         '1' => 'text-xl font-normal',
         default => null
     };
+
+    $showAllStyle = '';
+    $showAllStyle = match($colorPalette) {
+        '1' => 'text-red-700 hover:text-red-800',
+        '2' => 'text-blue-700 hover:text-blue-800',
+        '3' => 'text-rose-700 hover:text-rose-800',
+        '4' => 'text-zinc-700 hover:text-zinc-800',
+        '5' => 'text-cobalt-700 hover:text-cobalt-800',
+        default => null
+    };
 @endphp
 
 <section class="mb-4 lg:mb-12 relative z-[1] {{ $type == 2 ? 'lg:default_container' : 'default_container' }}">
@@ -12,7 +22,7 @@
     <div class="flex items-center justify-center {{ $type == 2 ? 'lg:mb-4' : 'mb-4' }} sm:justify-between px-4 lg:px-4">
         <h3 class=" text-gray-900 dark:text-white"> اطلاعیه ها </h3>
         <a href="#"
-            class="items-center hidden gap-2 text-xs font-normal text-red-700 sm:flex dark:text-red-600">
+            class="items-center hidden gap-2 text-xs font-normal sm:flex {{ $showAllStyle }}">
             <span> نمایش همه </span>
             <svg class="stroke-current" width="20" height="20" viewBox="0 0 20 20" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
--- resources/views/landing/page-single.blade.php
+++ resources/views/landing/page-single.blade.php
@@ -17,21 +17,20 @@
         @endif
 
         {{-- products --}}
-        <x-home_landing.productCategories radius="8"/>
+        <x-home_landing.productCategories colorPalette="{{ $land->styles->color }}" radius="{{ $land->styles->radius }}" />
 
         {{-- favorites --}}
         {{-- :type="$land->styles->product_type" --}}
-        <x-home_landing.products :landSlug="$land->slug" :data="$land->products" :type="$land->styles->product_type" colorPalette="{{ $land->styles->color }}" evenOdd="false" radius="8"
+        <x-home_landing.products :landSlug="$land->slug" :data="$land->products" :type="$land->styles->product_type" colorPalette="{{ $land->styles->color }}" evenOdd="false" radius="{{ $land->styles->radius }}"
             titleColor="title_color_type_1" />
 
         {{-- @dd($land->styles->article_type); --}}
-        {{-- @dd($land->styles); --}}
         {{-- notifications --}}
         {{--
             type 1 for list
             type 2 for tail
         --}}
-        <x-home_landing.announcement :landSlug="$land->slug" :data="$land->articles" type="{{ $land->styles->article_type }}" fontFamily="1" colorPalette="{{ $land->styles->color }}" radius="8"/>
+        <x-home_landing.announcement :landSlug="$land->slug" :data="$land->articles" type="{{ $land->styles->article_type }}" fontFamily="1" colorPalette="{{ $land->styles->color }}" radius="{{ $land->styles->radius }}" />
 
         {{-- videos --}}
         <x-home_landing.videos radius="12"/>
