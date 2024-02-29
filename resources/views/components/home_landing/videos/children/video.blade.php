b5e5f32069a2c92b247f74a1e70ebe322bb2e016
themestermim <themestermim@gmail.com>
2024-02-29 00:28:27 -0800

    start testing videos.

--- resources/views/components/home_landing/videos/children/video.blade.php
+++ resources/views/components/home_landing/videos/children/video.blade.php
@@ -26,16 +26,14 @@
     // };
     // console.log({!! $link !!});
 
-    // function renderVideo(event){
+    // const renderVideo(event){
     //     console.log(event);
     // }
 </script>
     
 @endpush
-{{-- @dd($link) --}}
-{{-- <x-splade-data default="{ showVideoModal: false }"> --}}
     <li class="">
-        <div class="relative w-full pt-[66%] cursor-pointer {{ $radiusSize }} overflow-hidden drop-shadow-base" @click="data.showVideoModal = true; data.videoSrc='{!! $link !!}'; this.renderVideo">
+        <div class="relative w-full pt-[66%] cursor-pointer {{ $radiusSize }} overflow-hidden drop-shadow-base videoThumbnails" @click="data.showVideoModal = true; data.videoSrc='{!! $link !!}'">
             <img src="{{ $src }}" alt="{{ $name }}" class="absolute top-0 left-0 w-full h-full object-cover z-[1]" />
             <x-icons.playIcon class="w-12 h-12 sm:w-20 sm:h-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[3]" />
             <div class="absolute text-white bottom-0 left-0 w-full z-[2] flex flex-col justify-end px-4 pb-5 bg-gradient-to-t from-black to-transparent h-1/2">
@@ -45,9 +43,3 @@
 
     </li>
     
-{{-- </x-splade-data> --}}
-{{-- <x-splade-script>
-    function renderVideo(event){
-        console.log(event);
-    }
-</x-splade-script> --}}
