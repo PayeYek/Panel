e2716d6be257a4f15750273070f0dfff275c5033
user350 <user350@activeserver.local>
2024-03-02 16:35:38 +0330

    first view of pdp finished.

--- resources/views/components/home_landing/videos/index.blade.php
+++ resources/views/components/home_landing/videos/index.blade.php
@@ -55,9 +55,9 @@
     </style>
 @endpush
 {{-- @dd($data); --}}
-<section class=" mb-4 lg:mb-20 relative z-[2] default_container">
+<section class=" mb-4 lg:mb-20 relative z-[2] lg:default_container">
     {{-- header --}}
-    <div class="flex items-center justify-between mb-4 lg:px-4">
+    <div class="flex items-center justify-between mb-4 px-4">
         <h3 class="text-lg font-bold text-gray-900"> ویدیو ها </h3>
         <a href="#" class="flex items-center gap-2 text-xs font-normal {{ $showAllStyle }}">
             <span> نمایش همه </span>
@@ -69,14 +69,14 @@
             </svg>
         </a>
     </div>
-    <ul class="grid grid-cols-1 gap-4 list-none lg:grid-cols-3">
+    <ul class="flex items-center lg:grid px-4 overflow-auto lg:overflow-visible lg:px-0 gap-4 list-none lg:grid-cols-3">
         @foreach ($data->take(3) as $video)
-            <li class="" data-videoLink="{{ $video->link }}">
+            <li class="w-64 sm:w-80 md:w-96 flex-none lg:w-full" data-videoLink="{{ $video->link }}">
                 <div class="relative w-full pt-[66%] cursor-pointer {{ $radiusSize }} overflow-hidden drop-shadow-base videoThumbnails" onclick="showVideoByThumbnail(this)">
                     <img src="{{ $video->image }}" alt="{{ $video->alt }}" class="absolute top-0 left-0 w-full h-full object-cover z-[1]" />
                     <x-icons.playIcon class="w-12 h-12 sm:w-20 sm:h-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[3]" />
                     <div class="absolute text-white bottom-0 left-0 w-full z-[2] flex flex-col justify-end px-4 pb-5 bg-gradient-to-t from-black to-transparent h-1/2">
-                        <p class="text-base sm:text-xl lg:text-base font-bold w-full"> {{ $video->alt }} </p>
+                        <p class="text-base sm:text-xl lg:text-base font-bold w-full line-clamp-2"> {{ $video->alt }} </p>
                     </div>
                 </div>
 
--- resources/views/landing/pdp.blade.php
+++ resources/views/landing/pdp.blade.php
@@ -19,11 +19,12 @@
     //     default => null
     // };
 @endphp
-@dd($breadcrumbs)
+
+{{-- @dd($product->toArray()) --}}
 <x-layout.default.main :land="$land">
     <main class="pt-4">
         {{-- breadcrumbs --}}
-        <ul class="default_container flex items-center text-[10px] gap-1.5 text-gray-900 mb-4">
+        <ul class="default_container flex items-center text-[10px] sm:text-xs gap-1.5 text-gray-900 mb-4">
             <li class="flex items-center gap-1.5">
                 <a href="#">
                     خانه
@@ -57,70 +58,183 @@
         </ul>
 
         {{-- product detail --}}
-        <section class="grid grid-cols-1 gap-4 default_container mb-8">
+        <section class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 lg:gap-12 xl:gap-16 default_container mb-8 lg:mb-12 xl:mb-16">
             {{-- product images --}}
-            <section class="w-full pt-[72%] relative">
-                <div class="absolute inset-0">
-                    <img src="/" alt="/" class="w-full h-full object-cover" />
+            <section class="md:flex flex-col gap-3">
+                <div class="w-full pt-[100%] relative">
+                    <div class="absolute inset-0">
+                        <img src="{{ $product->image }}" alt="{{ $product->name }}" class="w-full h-full object-cover {{ $radiusSize }}" />
+                    </div>
+                </div>
+
+                {{-- image thumbnails --}}
+                <div class="md:grid hidden grid-cols-3 gap-3">
+                    <div class="aspect-square w-full cursor-pointer bg-gray-300 {{ $radiusSize }}">
+                        <img src="/" alt="thumbnail" class="w-full h-full {{ $radiusSize }} object-cover" />
+                    </div>
+                    <div class="aspect-square w-full cursor-pointer bg-gray-300 {{ $radiusSize }}">
+                        <img src="/" alt="thumbnail" class="w-full h-full {{ $radiusSize }} object-cover" />
+                    </div>
+                    <div class="aspect-square w-full cursor-pointer bg-gray-300 {{ $radiusSize }}">
+                        <img src="/" alt="thumbnail" class="w-full h-full {{ $radiusSize }} object-cover" />
+                    </div>
                 </div>
             </section>
 
             {{-- info --}}
             <section class="">
+                <p class="hidden md:block text-2xl lg:text-[32px] font-medium text-red-700 mb-8 lg:mb-11 line-clamp-1"> {{ $product->name }} </p>
                 {{-- boxes --}}
-                <div class="grid grid-cols-3 gap-3 text-sm font-normal mb-4">
+                <div class="grid grid-cols-3 gap-3 text-sm font-normal mb-4 md:max-w-[524px]">
                     <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 {{ $radiusSize }}">
-                        <p class="text-red-700"> سیستم ترمز </p>
-                        <p class="text-gray-900"> کاسه ای </p>
+                        <p class="text-red-700 line-clamp-1"> سیستم ترمز </p>
+                        <p class="text-gray-900 line-clamp-1"> کاسه ای </p>
                     </div>
                     <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 {{ $radiusSize }}">
-                        <p class="text-red-700"> سیستم ترمز </p>
-                        <p class="text-gray-900"> کاسه ای </p>
+                        <p class="text-red-700 line-clamp-1"> سیستم ترمز </p>
+                        <p class="text-gray-900 line-clamp-1"> کاسه ای </p>
                     </div>
                     <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 {{ $radiusSize }}">
-                        <p class="text-red-700"> سیستم ترمز </p>
-                        <p class="text-gray-900"> کاسه ای </p>
+                        <p class="text-red-700 line-clamp-1"> سیستم ترمز </p>
+                        <p class="text-gray-900 line-clamp-1"> کاسه ای </p>
                     </div>
                     <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 {{ $radiusSize }}">
-                        <p class="text-red-700"> سیستم ترمز </p>
-                        <p class="text-gray-900"> کاسه ای </p>
+                        <p class="text-red-700 line-clamp-1"> سیستم ترمز </p>
+                        <p class="text-gray-900 line-clamp-1"> کاسه ای </p>
                     </div>
                     <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 {{ $radiusSize }}">
-                        <p class="text-red-700"> سیستم ترمز </p>
-                        <p class="text-gray-900"> کاسه ای </p>
+                        <p class="text-red-700 line-clamp-1"> سیستم ترمز </p>
+                        <p class="text-gray-900 line-clamp-1"> کاسه ای </p>
                     </div>
                     <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 {{ $radiusSize }}">
-                        <p class="text-red-700"> سیستم ترمز </p>
-                        <p class="text-gray-900"> کاسه ای </p>
+                        <p class="text-red-700 line-clamp-1"> سیستم ترمز </p>
+                        <p class="text-gray-900 line-clamp-1"> کاسه ای </p>
                     </div>
                 </div>
 
                 {{-- guide btns --}}
-                <div class="flex_center flex-col gap-2">
+                <div class="flex_center flex-col gap-2 md:max-w-[524px] md:flex-row lg:gap-4 ">
                     <LandBtn to="/" classNames="h-11 w-[254px] bg-white border border-red-700 text-red-700 text-base font-bold flex_center {{ $radiusSize }}" text="دانلود کاتالوگ" />
                     <LandBtn to="/" classNames="h-11 w-[254px] bg-red-700 text-white text-base font-bold flex_center {{ $radiusSize }}" text="مشاوره و خرید" />
                 </div>
             </section>
         </section>
 
-        {{-- tabs --}}
-        <div class="overflow-hidden sticky top-16 mb-4 z-[1] bg-white pt-2">
-            <div class="overflow-auto flex">
-                <ul class="flex-none text-sm mx-4 font-normal text-gray-900 gap-10 flex items-center border-b-2 border-[#e7e8e9] l_tab_styles">
-                    <li class="flex-none py-2.5 cursor-pointer relative before:absolute before:content-[''] before:-bottom-0.5 before:inset-x-0 before:h-1 before:w-full before:rounded-lg before:bg-red-700 before:hidden active"> مشخصات فنی </li>
-                    <li class="flex-none py-2.5 cursor-pointer relative before:absolute before:content-[''] before:-bottom-0.5 before:inset-x-0 before:h-1 before:w-full before:rounded-lg before:bg-red-700 before:hidden"> توضیحات تکمیلی </li>
-                    <li class="flex-none py-2.5 cursor-pointer relative before:absolute before:content-[''] before:-bottom-0.5 before:inset-x-0 before:h-1 before:w-full before:rounded-lg before:bg-red-700 before:hidden"> دیدگاه شما </li>
-                    <li class="flex-none py-2.5 cursor-pointer relative before:absolute before:content-[''] before:-bottom-0.5 before:inset-x-0 before:h-1 before:w-full before:rounded-lg before:bg-red-700 before:hidden"> سوالات متداول </li>
-                </ul>
-            </div>
-        </div>
+        <x-splade-data default="{ activeTab: 1 }">
+            <section class="mb-4">
+                {{-- tabs --}}
+                <div class="overflow-hidden sticky top-16 lg:top-20 mb-4 z-[1] bg-white pt-2 sm:default_container">
+                    <div class="overflow-auto flex sm:border-b-2 sm:border-[#e7e8e9] sm:overflow-visible">
+                        <ul class="flex-none text-sm lg:text-base sm:gap-12 md:gap-14 lg:gap-10 mx-4 sm:mx-0 font-normal text-gray-900 gap-10 flex items-center border-b-2 border-[#e7e8e9] sm:border-b-0 l_tab_styles">
+                            <li class="flex-none lg:pl-4 xl:pl-8 duration-0 lg:py-3 py-2.5 cursor-pointer relative before:absolute before:content-[''] before:-bottom-0.5 before:inset-x-0 before:h-1 before:w-full before:rounded-lg before:bg-red-700 before:hidden" @click="data.activeTab = 1" v-bind:class="{'active' : data.activeTab == 1 }"> مشخصات فنی </li>
+                            <li class="flex-none lg:pl-4 xl:pl-8 duration-0 lg:py-3 py-2.5 cursor-pointer relative before:absolute before:content-[''] before:-bottom-0.5 before:inset-x-0 before:h-1 before:w-full before:rounded-lg before:bg-red-700 before:hidden" @click="data.activeTab = 2" v-bind:class="{'active' : data.activeTab == 2 }"> توضیحات تکمیلی </li>
+                            <li class="flex-none lg:pl-4 xl:pl-8 duration-0 lg:py-3 py-2.5 cursor-pointer relative before:absolute before:content-[''] before:-bottom-0.5 before:inset-x-0 before:h-1 before:w-full before:rounded-lg before:bg-red-700 before:hidden" @click="data.activeTab = 3" v-bind:class="{'active' : data.activeTab == 3 }"> دیدگاه شما </li>
+                            <li class="flex-none lg:pl-4 xl:pl-8 duration-0 lg:py-3 py-2.5 cursor-pointer relative before:absolute before:content-[''] before:-bottom-0.5 before:inset-x-0 before:h-1 before:w-full before:rounded-lg before:bg-red-700 before:hidden" @click="data.activeTab = 4" v-bind:class="{'active' : data.activeTab == 4 }"> سوالات متداول </li>
+                        </ul>
+                    </div>
+                </div>
 
-        {{-- Technical Specifications --}}
-        <section class="default_container">
-            <ul class="flex flex-col gap-2 text-gray-900">
-                <x-pdp.TechnicalSpecifications radius="{{ $radiusSize }}" />
-            </ul>
-        </section>
+                {{-- Technical Specifications --}}
+                <section class="default_container" v-show="data.activeTab == 1">
+                    <ul class="flex flex-col gap-2 text-gray-900">
+                        <x-pdp.TechnicalSpecifications radius="{{ $radiusSize }}" />
+                    </ul>
+                </section>
+                
+                {{-- Further Details --}}
+                <section class="default_container" v-show="data.activeTab == 2">
+                    <ul class="flex flex-col gap-2 text-gray-900">
+                        <x-splade-data default="{ toggle: true }">
+                            <li class="p-4 drop-shadow-base bg-white {{ $radiusSize }} lg:pr-16 lg:pl-8 xl:pr-28">
+                                {{-- title --}}
+                                <div class="flex items-center justify-between">
+                                    <p class="text-sm lg:text-base font-bold"> بررسی اجمالی </p>
+                                    <button type="button" class="cursor-pointer" @click="data.toggle = !data.toggle">
+                                        <svg class="duration-1000" :class="data.toggle ? 'rotate-180' : 'rotate-0'" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M7 10L12.0008 14.58L17 10" stroke="#111827" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                    </button>
+                                </div>
+        
+                                <div class="duration-1000 overflow-hidden lg:pr-2" v-bind:class="data.toggle ? 'max-h-96 pb-4 mt-4' : 'max-h-0 pb-0'">
+                                    <p class="text-sm lg:text-base font-normal pb-4">
+                                        در سال 1402  قدرتمندترین کامیون کمپرسی جفت
+                                        محور‌ بازار با نام شکموتو ایکس 5000D با قدرت موتور 450 اسب بخار رونمایی شد و توجه بسیاری را به خود جلب کرد. این خودرو طراحی داخلی منحصر به فردی دارد و با وجود یک تخت خواب، راحتی را برای سرنشین خود تضمین می‌کند.
+            
+                                         SHACMOTO
+                                        X5000-D با قابلیت کروز کنترل و فرمان چندمنظوره به
+                                            راحتی رقبای خود را کنار می‌زند. در کنار تمام این مزایا، این خودرو با داشتن
+                                            سیستم آلایندگی EURO 5 EEV به
+                                        کاهش آلودگی هوا نیز کمک می‌کند. از دیگر امکانات این خودروی 26 تنی می‌توان به دوربین 360 درجه و سقف شیشه‌ای اشاره کرد. در ادامه اطلاعات تکمیلی این
+                                        کامیون بی‌نظیر را بخوانید.
+                                    </p>
+                                </div>
+                            </li>
+                        </x-splade-data>
+                    </ul>
+                </section>
+        
+                {{-- viewpoint --}}
+                <section class="default_container" v-show="data.activeTab == 3">
+                    <ul class="flex flex-col gap-7 text-gray-900">
+                        <x-splade-data default="{ toggle: false }">
+                            <li class="p-4 drop-shadow-base bg-white {{ $radiusSize }} lg:pr-16 lg:pl-8 xl:pr-28">
+                                {{-- user name --}}
+                                <p class="block text-sm leading-6 lg:leading-7 lg:text-base font-bold text-[#585d68] border-b border-[#e7e8e9] mb-3 pb-0.5"> مهران محمودی </p>
+                                {{-- user viewpoint --}}
+                                <div class="flex items-center justify-between gap-10">
+                                    <p class="text-normal leading-6 lg:leading-7 text-justify text-sm lg:text-base"> 
+                                        سلام خسته نباشید. قیمت نقدی این محصول چقدره و چند روزه تحویل میدید؟
+                                    </p>
+                                    <button type="button" class="cursor-pointer" @click="data.toggle = !data.toggle">
+                                        <svg class="duration-1000" :class="data.toggle ? 'rotate-180' : 'rotate-0'" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M7 10L12.0008 14.58L17 10" stroke="#111827" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                    </button>
+                                </div>
+        
+                                <div class="duration-1000 overflow-hidden lg:pr-2" v-bind:class="data.toggle ? 'max-h-96 pb-4 mt-4' : 'max-h-0 pb-0'">
+                                    <p class="text-sm leading-6 lg:text-base lg:leading-7 text-justify font-normal pb-4 bg-gray-100 py-2 px-2.5 {{ $radiusSize }}">
+                                        کامیونت 8.5 تن جک یا N721-N85 از تولیدات شرکت جک موتورز چین است که توسط آرین دیزل مونتاژ و به بازار عرضه می‌شود. این خودرو با داشتن قدرت موتور 155 اسب بخار و تناژ 8.5 تن برای مسیرهای بین شهری مناسب است. در طی سال‌های اخیر، کامیونت‌های جک مورد توجه بخش بزرگی از بازار قرار گرفته زیرا این خودروها دارای قدرت بالا و استهلاک کمی هستند. کامیونت 8.5 تن جک با داشتن جای خواب و صندلی دونفره شاگرد، مسیر سفر را برای سرنشینان خود هموار می‌کند. برای اطلاعات بیشتر در ادامه با ما همراه باشید.
+                                    </p>
+                                </div>
+                            </li>
+                        </x-splade-data>
+                    </ul>
+                </section>
+
+                {{-- faq --}}
+                <section class="default_container" v-show="data.activeTab == 4">
+                    <ul class="flex flex-col gap-7 text-gray-900">
+                        <x-splade-data default="{ toggle: false }">
+                            <li class="p-4 drop-shadow-base bg-white {{ $radiusSize }} lg:pr-16 lg:pl-8 xl:pr-28">
+                                {{-- user name --}}
+                                <p class="block text-sm leading-6 lg:leading-7 lg:text-base font-bold text-[#585d68] border-b border-[#e7e8e9] mb-3 pb-0.5"> مهران محمودی </p>
+                                {{-- user viewpoint --}}
+                                <div class="flex items-center justify-between gap-10">
+                                    <p class="text-normal leading-6 lg:leading-7 text-justify text-sm lg:text-base"> 
+                                        سلام خسته نباشید. قیمت نقدی این محصول چقدره و چند روزه تحویل میدید؟
+                                    </p>
+                                    <button type="button" class="cursor-pointer" @click="data.toggle = !data.toggle">
+                                        <svg class="duration-1000" :class="data.toggle ? 'rotate-180' : 'rotate-0'" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M7 10L12.0008 14.58L17 10" stroke="#111827" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                    </button>
+                                </div>
+        
+                                <div class="duration-1000 overflow-hidden lg:pr-2" v-bind:class="data.toggle ? 'max-h-96 pb-4 mt-4' : 'max-h-0 pb-0'">
+                                    <p class="text-sm leading-6 lg:text-base lg:leading-7 text-justify font-normal pb-4 bg-gray-100 py-2 px-2.5 {{ $radiusSize }}">
+                                        کامیونت 8.5 تن جک یا N721-N85 از تولیدات شرکت جک موتورز چین است که توسط آرین دیزل مونتاژ و به بازار عرضه می‌شود. این خودرو با داشتن قدرت موتور 155 اسب بخار و تناژ 8.5 تن برای مسیرهای بین شهری مناسب است. در طی سال‌های اخیر، کامیونت‌های جک مورد توجه بخش بزرگی از بازار قرار گرفته زیرا این خودروها دارای قدرت بالا و استهلاک کمی هستند. کامیونت 8.5 تن جک با داشتن جای خواب و صندلی دونفره شاگرد، مسیر سفر را برای سرنشینان خود هموار می‌کند. برای اطلاعات بیشتر در ادامه با ما همراه باشید.
+                                    </p>
+                                </div>
+                            </li>
+                        </x-splade-data>
+                    </ul>
+                </section>
+            </section>
+        </x-splade-data>
+
+        <x-home_landing.videos colorPalette="{{ $land->styles->color }}" radius="{{ $land->styles->radius }}" :data="$land->videos" />
     </main>
 
 </x-layout.default.main>
