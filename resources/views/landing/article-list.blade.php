9caef3e0a428a03a7da5e78e002102aecd7605af
themestermim <themestermim@gmail.com>
2024-03-05 12:51:55 +0330

    start working on articles.

--- resources/views/landing/article-list.blade.php
+++ resources/views/landing/article-list.blade.php
@@ -1,7 +1,7 @@
-<x-layout.landing.land :land="$land">
+<x-layout.default.main :land="$land">
 
-    <x-layout.landing.sidebar :land="$land"/>
+    {{-- <x-layout.landing.sidebar :land="$land"/> --}}
 
-    <x-layout.landing.articles :land="$land" all/>
+    {{-- <x-layout.landing.articles :land="$land" all/> --}}
 
-</x-layout.landing.land>
+</x-layout.default.main>
