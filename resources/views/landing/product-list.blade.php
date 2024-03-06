3571ed0b7ef5b7e205379187110c50dc73ec1e02
themestermim <themestermim@gmail.com>
2024-03-06 10:35:17 +0330

    start make colors modular.

--- resources/views/landing/product-list.blade.php
+++ resources/views/landing/product-list.blade.php
@@ -29,13 +29,20 @@
     };
 @endphp
 
+@push('script')
+    <script>
+        const selectFilter = document.getElementById("selectFilter")
+        console.log(selctFilter);
+    </script>
+@endpush
+
 <x-layout.default.main :land="$land">
 
     <main class="pt-4 relative">
         <section class="default_container mb-8 lg:flex lg:items-center lg:gap-4">
             <p class="mb-4 lg:mb-0 text-base font-bold text-gray-900 text-center"> محصولات </p>
             <div class="h-10 w-full max-w-96 mx-auto lg:w-36 lg:mx-0 before:absolute before:content-[''] before:w-2 before:h-2 before:border-r-2 before:border-b-2 {{ $selectBeforeStyle }} before:top-1/2 before:left-4 before:-translate-y-1/2 before:rotate-45 relative">
-                <select class="w-full h-full border focus:ring-0 outline-none !bg-none {{ $selectStyle }} {{ $radiusSize }}">
+                <select id="selectFilter" class="w-full h-full border focus:ring-0 outline-none !bg-none {{ $selectStyle }} {{ $radiusSize }}">
                     <option value="1"> کامیون </option>
                 </select>
             </div>
