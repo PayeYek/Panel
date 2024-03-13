2a9cec3558c37d0a8f05558713b40760d9138ac8
themestermim <themestermim@gmail.com>
2024-03-13 10:15:55 +0330

    sidebar fixed

--- resources/views/components/layout/landing/header.blade.php
+++ resources/views/components/layout/landing/header.blade.php
@@ -1,46 +1,35 @@
 @props(['land' => null])
 
 <x-splade-data store="navigation" default="{ opened: false }" />
-@if ($land)
-    <header class="sticky top-0 z-[4] drop-shadow-[0_4px_4px_rgba(0,0,0,0.15)] bg-white h-16 sm:h-20">
-        <section class="flex items-center justify-between h-full gap-5 default_container">
-            <x-layout.landing.logo :land="$land" />
+    @if ($land)
+        <header class="sticky top-0 z-[4] drop-shadow-[0_4px_4px_rgba(0,0,0,0.15)] bg-white h-16 sm:h-20">
+            <section class="flex items-center justify-between h-full gap-5 default_container">
+                <div class="flex items-center gap-2">
+                    {{-- hamburger menu --}}
+                    <button @click.prevent="navigation.opened = !navigation.opened"
+                        class="p-1 text-gray-600 rounded-lg cursor-pointer md:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 dark:text-gray-400">
+                        <svg class="size-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_903_3398)">
+                            <path d="M20 17.5C20.3852 17.5002 20.7556 17.6486 21.0344 17.9144C21.3132 18.1802 21.479 18.5431 21.4975 18.9279C21.516 19.3127 21.3858 19.6898 21.1338 19.9812C20.8818 20.2726 20.5274 20.4558 20.144 20.493L20 20.5H4C3.61478 20.4998 3.24441 20.3514 2.96561 20.0856C2.68682 19.8198 2.52099 19.4569 2.50248 19.0721C2.48396 18.6873 2.61419 18.3102 2.86618 18.0188C3.11816 17.7274 3.47258 17.5442 3.856 17.507L4 17.5H20ZM20 10.5C20.3978 10.5 20.7794 10.658 21.0607 10.9393C21.342 11.2206 21.5 11.6022 21.5 12C21.5 12.3978 21.342 12.7794 21.0607 13.0607C20.7794 13.342 20.3978 13.5 20 13.5H4C3.60218 13.5 3.22064 13.342 2.93934 13.0607C2.65804 12.7794 2.5 12.3978 2.5 12C2.5 11.6022 2.65804 11.2206 2.93934 10.9393C3.22064 10.658 3.60218 10.5 4 10.5H20ZM20 3.5C20.3978 3.5 20.7794 3.65804 21.0607 3.93934C21.342 4.22064 21.5 4.60218 21.5 5C21.5 5.39782 21.342 5.77936 21.0607 6.06066C20.7794 6.34196 20.3978 6.5 20 6.5H4C3.60218 6.5 3.22064 6.34196 2.93934 6.06066C2.65804 5.77936 2.5 5.39782 2.5 5C2.5 4.60218 2.65804 4.22064 2.93934 3.93934C3.22064 3.65804 3.60218 3.5 4 3.5H20Z" fill="black"/>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_903_3398">
+                            <rect width="24" height="24" fill="white"/>
+                            </clipPath>
+                            </defs>
+                        </svg>
+                        <span class="sr-only">Toggle sidebar</span>
+                    </button>
+                    <x-layout.landing.logo :land="$land" />
+                </div>
 
-            <nav class="flex-1 hidden md:flex text-sm font-medium px-3 py-2.5 text-gray-900">
-                <a class="px-3 hover:text-focus"
-                    href="{{ route('landing.page.show', ['page' => $land->slug]) }}">{{ __('Home') }}</a>
-                <a class="px-3 hover:text-focus"
-                    href="{{ route('landing.product.list', ['page' => $land->slug]) }}">{{ __('Products') }}</a>
-                <a class="px-3 hover:text-focus"
-                    href="{{ route('landing.sales', ['page' => $land->slug]) }}">{{ __('Sales Agency') }}</a>
-                <a class="px-3 hover:text-focus"
-                    href="{{ route('landing.page.about', ['page' => $land->slug]) }}">{{ __('About us') }}</a>
-            </nav>
+                <x-layout.landing.navbar :land="$land" class-names="hidden md:flex" />
 
-            <div class="flex items-center gap-2.5">
-                {{-- <SwitchStyle class="hidden sm:block" /> --}}
-                {{-- <Breakpoint class="shrink-0 {{ app()->environment('production') ? 'hidden' : '' }}" /> --}}
-                <Link class="w-24 h-8 px-2 text-xs font-bold text-white bg-normal hover:bg-focus focus:bg-focus focus:shadow-focus focus:shadow-shadowNormal rounded-custom flex_center"
-                    href="{{ route('landing.page.show', ['page' => $land->slug]) }}">{{ __('Terms of sale') }}</Link>
-
-                {{-- hamburger menu --}}
-                <button @click.prevent="navigation.opened = !navigation.opened"
-                    class="p-2 text-gray-600 rounded-lg cursor-pointer md:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                    <svg aria-hidden="true" :class="{ 'hidden': navigation.opened }" class="w-6 h-6" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                    <svg aria-hidden="true" :class="{ 'hidden': !navigation.opened }" class="w-6 h-6" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                    <span class="sr-only">Toggle sidebar</span>
-                </button>
-            </div>
-        </section>
-    </header>
-@endif
+                    {{-- <SwitchStyle class="hidden sm:block" /> --}}
+                    {{-- <Breakpoint class="shrink-0 {{ app()->environment('production') ? 'hidden' : '' }}" /> --}}
+                    {{-- term of sale --}}
+                    <Link class="w-24 h-8 px-2 text-xs font-bold text-white bg-normal hover:bg-focus focus:bg-focus focus:shadow-focus focus:shadow-shadowNormal rounded-custom flex_center"
+                        href="{{ route('landing.page.show', ['page' => $land->slug]) }}">{{ __('Terms of sale') }}</Link>
+            </section>
+        </header>
+    @endif
--- resources/views/components/layout/landing/sidebar.blade.php
+++ resources/views/components/layout/landing/sidebar.blade.php
@@ -2,24 +2,19 @@
 {{--SIDEBAR--}}
 <aside
     :class="navigation.opened ? 'translate-x-0' : 'ltr:translate-x-[-100%] rtl:translate-x-[100%]'"
-    class="fixed top-16 sm:top-20 ltr:left-0 rtl:right-0 z-40 w-64 h-screen pt-4 transition-transform -translate-x-full bg-white border-r border-gray-200 ltr:md:translate-x-0 rtl:md:translate-x-0 md:hidden dark:bg-gray-800 dark:border-gray-700"
+    class="fixed top-0 ltr:left-0 rtl:right-0 z-[6] flex flex-col pb-5 px-4 w-64 h-screen pt-6 transition-transform -translate-x-full bg-white border-r border-gray-200 ltr:md:translate-x-0 rtl:md:translate-x-0 md:hidden"
 >
-    <div
-        class="scrollbar-thin scrollbar-thumb-gray-200 scrollbar-track-white dark:scrollbar-thumb-gray-700 dark:scrollbar-track-gray-800 overflow-y-auto pb-5 px-3 h-full bg-white dark:bg-gray-800">
-
-        <ul class="space-y-2 w-full">
-            <li class="flex"><a class="bg-gray-100 rounded-lg w-full py-1.5 px-4 hover:text-red-500 transition-all duration-100" href="{{ route('landing.page.show', ['page'=>$land->slug]) }}">{{__('Home')}}</a></li>
-            <li class="flex"><a class="bg-gray-100 rounded-lg w-full py-1.5 px-4 hover:text-red-500 transition-all duration-100" href="{{ route('landing.product.list', ['page' => $land->slug]) }}">{{__('Products')}}</a></li>
-            <li class="flex"><a class="bg-gray-100 rounded-lg w-full py-1.5 px-4 hover:text-red-500 transition-all duration-100" href="{{ route('landing.page.show', ['page'=>$land->slug]) }}">{{__('Sales Agency')}}</a></li>
-            <li class="flex"><a class="bg-gray-100 rounded-lg w-full py-1.5 px-4 hover:text-red-500 transition-all duration-100" href="{{ route('landing.page.about', ['page'=>$land->slug]) }}">{{__('About us')}}</a></li>
-        </ul>
-
-        <ul class="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
-            <x-layout.landing.social colorPalette="{{ $land->styles->color }}" />
-        </ul>
-
-    </div>
+    {{-- close menu --}}
+    <button type="button" class="p-1 mb-2 mr-auto">
+        <svg class="size-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 12L6 6M12 12L18 18M12 12L18 6M12 12L6 18" stroke="#58595B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    </button>
+    
+    <x-layout.landing.navbar :land="$land" class-names="flex" />
+    
+    <x-layout.landing.social />
 </aside>
 {{--Sidebar - Mobile Shadow--}}
 <div v-if="navigation.opened" @click.prevent="navigation.opened = !navigation.opened"
-     class="bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-x-0 bottom-0 top-16 sm:top-20 z-30 md:hidden"></div>
+     class="bg-gray-900 bg-opacity-50 fixed inset-x-0 bottom-0 top-0 z-[5] md:hidden"></div>
