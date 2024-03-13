fe890bb5c09907adc9238ff5feae91dc5d076c91
themestermim <themestermim@gmail.com>
2024-03-13 10:22:50 +0330

    some bugs fixed

--- resources/views/components/layout/landing/navbar.blade.php
+++ resources/views/components/layout/landing/navbar.blade.php
@@ -3,9 +3,9 @@
     'classNames' => '',
 ])
 
-<nav :class="'text-slate-700 text-base mb-16 font-bold *:h-10 *:px-4 *:rounded-custom *:bg-[#f5f5f5] *:w-full *:flex *:items-center flex-col gap-4 {{ $classNames }}'">
-    <Link class="focus:bg-[#eaeaea]" href="{{ route('landing.page.show', ['page'=>$land->slug]) }}">{{__('Home')}}</Link>
-    <Link class="focus:bg-[#eaeaea]" href="{{ route('landing.product.list', ['page' => $land->slug]) }}">{{__('Products')}}</Link>
-    <Link class="focus:bg-[#eaeaea]" href="{{ route('landing.page.show', ['page'=>$land->slug]) }}">{{__('Sales Agency')}}</Link>
-    <Link class="focus:bg-[#eaeaea]" href="{{ route('landing.page.about', ['page'=>$land->slug]) }}">{{__('About us')}}</Link>
+<nav :class="'text-slate-700 text-base mb-16 *:h-10 *:px-4 *:rounded-custom *:bg-[#f5f5f5] *:w-full *:flex *:items-center flex-col gap-4 {{ $classNames }}'">
+    <Link class="focus:bg-[#eaeaea] font-medium hover:font-bold focus:font-bold" href="{{ route('landing.page.show', ['page'=>$land->slug]) }}">{{__('Home')}}</Link>
+    <Link class="focus:bg-[#eaeaea] font-medium hover:font-bold focus:font-bold" href="{{ route('landing.product.list', ['page' => $land->slug]) }}">{{__('Products')}}</Link>
+    <Link class="focus:bg-[#eaeaea] font-medium hover:font-bold focus:font-bold" href="{{ route('landing.page.show', ['page'=>$land->slug]) }}">{{__('Sales Agency')}}</Link>
+    <Link class="focus:bg-[#eaeaea] font-medium hover:font-bold focus:font-bold" href="{{ route('landing.page.about', ['page'=>$land->slug]) }}">{{__('About us')}}</Link>
 </nav>
