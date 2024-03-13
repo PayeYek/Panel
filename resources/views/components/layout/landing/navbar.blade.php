a94e2843e187b69125af9c49d46b79d89c821e58
themestermim <themestermim@gmail.com>
2024-03-13 10:29:34 +0330

    some bugs fixed

--- resources/views/components/layout/landing/navbar.blade.php
+++ resources/views/components/layout/landing/navbar.blade.php
@@ -3,7 +3,7 @@
     'classNames' => '',
 ])
 
-<nav :class="'text-slate-700 text-base mb-16 *:h-10 *:px-4 *:rounded-custom *:bg-[#f5f5f5] *:w-full *:flex *:items-center flex-col gap-4 {{ $classNames }}'">
+<nav :class="'text-slate-700 text-base mb-16 *:h-10 *:px-4 *:rounded-custom *:bg-[#f5f5f5] *:w-full *:flex *:items-center flex-col lg:flex-row lg:items-center lg:*:h-auto lg:*:px-0 lg:*:rounded-none lg:*:w-auto lg:*:bg-transparent lg:mb-0 gap-4 {{ $classNames }}'">
     <Link class="focus:bg-[#eaeaea] font-medium hover:font-bold focus:font-bold" href="{{ route('landing.page.show', ['page'=>$land->slug]) }}">{{__('Home')}}</Link>
     <Link class="focus:bg-[#eaeaea] font-medium hover:font-bold focus:font-bold" href="{{ route('landing.product.list', ['page' => $land->slug]) }}">{{__('Products')}}</Link>
     <Link class="focus:bg-[#eaeaea] font-medium hover:font-bold focus:font-bold" href="{{ route('landing.page.show', ['page'=>$land->slug]) }}">{{__('Sales Agency')}}</Link>
