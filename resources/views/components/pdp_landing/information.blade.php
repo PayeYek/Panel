935c126367e9366b6b8d0a7f3450f9759a322924
themestermim <themestermim@gmail.com>
2024-03-14 12:17:57 +0330

    pdp in mobile finished.

--- resources/views/components/pdp_landing/information.blade.php
+++ resources/views/components/pdp_landing/information.blade.php
@@ -3,33 +3,33 @@
     'product' => '',
 ])
 
-<section class="lg:pt-24">
+<section class="lg:pt-24 flex flex-col-reverse gap-4">
     <h1 class="hidden md:block text-2xl lg:text-[32px] font-medium text-normal mb-8 lg:mb-11 line-clamp-1"> {{ $productName }} </h1>
     {{-- boxes --}}
-    <div class="grid grid-cols-3 gap-3 text-sm 2xl:text-[15px] font-normal mb-4 md:max-w-[524px]">
-        <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 rounded-custom">
-            <p class="line-clamp-1 text-normal"> نوع کاربری </p>
-            <p class="text-gray-900 line-clamp-1 font-semibold"> {{ $product->usage }} </p>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-3 text-sm 2xl:text-[15px] md:max-w-[524px]">
+        <div class="md:aspect-square h-12 grid grid-cols-2 content-center px-3 bg-dark-50 gap-1 rounded-custom">
+            <p class="font-medium text-normal line-clamp-1"> نوع کاربری </p>
+            <p class="text-stone-700 line-clamp-1 font-normal"> {{ $product->usage }} </p>
         </div>
-        <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 rounded-custom">
-            <p class="text-normal line-clamp-1"> نوع کابین </p>
-            <p class="text-gray-900 line-clamp-1 font-semibold">  {{ $product->cabin == 0 ? 'بدون خواب' : 'خواب دار' }} </p>
+        <div class="md:aspect-square h-12 grid grid-cols-2 content-center p-1 bg-dark-50 gap-1 rounded-custom">
+            <p class="font-medium text-normal line-clamp-1"> نوع کابین </p>
+            <p class="text-stone-700 line-clamp-1 font-normal">  {{ $product->cabin == 0 ? 'بدون خواب' : 'خواب دار' }} </p>
         </div>
-        <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 rounded-custom">
-            <p class="text-normal line-clamp-1"> تناژ </p>
-            <p class="text-gray-900 line-clamp-1 font-semibold"> {{ $product->tonnage }} </p>
+        <div class="md:aspect-square h-12 grid grid-cols-2 content-center p-1 bg-dark-50 gap-1 rounded-custom">
+            <p class="font-medium text-normal line-clamp-1"> تناژ </p>
+            <p class="text-stone-700 line-clamp-1 font-normal"> {{ $product->tonnage }} </p>
         </div>
-        <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 rounded-custom">
-            <p class="text-normal line-clamp-1"> تعداد محور چرخ‌ها </p>
-            <p class="text-gray-900 line-clamp-1 font-semibold"> {{ $product->axle == 1 ? 'تک محوره' : ($product->axle == 2 ? 'جفت محوره' : 'سه محوره') }} </p>
+        <div class="md:aspect-square h-12 grid grid-cols-2 content-center p-1 bg-dark-50 gap-1 rounded-custom">
+            <p class="font-medium text-normal line-clamp-1"> تعداد محور چرخ‌ها </p>
+            <p class="text-stone-700 line-clamp-1 font-normal"> {{ $product->axle == 1 ? 'تک محوره' : ($product->axle == 2 ? 'جفت محوره' : 'سه محوره') }} </p>
         </div>
-        <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 rounded-custom">
-            <p class="text-normal line-clamp-1"> سال </p>
-            <p class="text-gray-900 line-clamp-1 font-semibold"> {{ $product->year }} </p>
+        <div class="md:aspect-square h-12 grid grid-cols-2 content-center p-1 bg-dark-50 gap-1 rounded-custom">
+            <p class="font-medium text-normal line-clamp-1"> سال </p>
+            <p class="text-stone-700 line-clamp-1 font-normal"> {{ $product->year }} </p>
         </div>
-        <div class="aspect-square flex_center flex-col p-1 bg-dark-50 gap-1 rounded-custom">
-            <p class="text-normal line-clamp-1"> مدل </p>
-            <p class="text-gray-900 line-clamp-1 font-semibold"> {{ $product->model }} </p>
+        <div class="md:aspect-square h-12 grid grid-cols-2 content-center p-1 bg-dark-50 gap-1 rounded-custom">
+            <p class="font-medium text-normal line-clamp-1"> مدل </p>
+            <p class="text-stone-700 line-clamp-1 font-normal"> {{ $product->model }} </p>
         </div>
     </div>
 
--- resources/views/landing/pdp.blade.php
+++ resources/views/landing/pdp.blade.php
@@ -8,143 +8,135 @@
 
         {{-- product detail --}}
         <section
-            class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 lg:gap-12 xl:gap-16 default_container mb-8 lg:mb-12 xl:mb-16">
+            class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 lg:gap-12 xl:gap-16 default_container mb-14 lg:mb-12 xl:mb-16">
             <h1 class="md:hidden text-xl font-bold text-stone-700 mb-5"> {{ $product->name }} </h1>
             {{-- product images --}}
             <section class="md:flex flex-col gap-3">
                 {{-- desktop slider --}}
-                <landPdpDesktopSlider name="{{ $product->name }}" mainImage="{{ $product->image }}" slides="{{ $product }}"/>
+                <landPdpDesktopSlider name="{{ $product->name }}" mainImage="{{ $product->image }}"
+                    slides="{{ $product }}" />
 
                 {{-- mobile slider --}}
-                <landPdpMobileSlider slides="{{ $product }}"/>
+                <landPdpMobileSlider slides="{{ $product }}" />
             </section>
 
             {{-- info --}}
             <x-pdp_landing.information productName="{{ $product->name }}" :product="$product" />
         </section>
 
-        <x-splade-data default="{ activeTab: 1 }">
-            <section class="mb-8 lg:mb-16">
-                {{-- tabs --}}
-                <x-pdp_landing.sectionTabs />
+        {{-- Further Details --}}
+        <section class="default_container mb-20">
+            <p
+                class="pr-4 relative text-xl mb-7 font-bold text-stone-700 before:top-1/2 before:right-0 before:-translate-y-1/2 before:bg-normal before:h-12 before:w-1 before:rounded-l-custom before:absolute before:content-['']">
+                درباره محصول </p>
+            <div class="pb-4 text-justify custom_article_styles">
+                {!! $product->body !!}
+            </div>
+        </section>
 
-                {{-- Further Details --}}
-                <section class="default_container" v-show="data.activeTab == 1">
-                    <ul class="flex flex-col gap-2 text-gray-900">
-                        <li class="p-4 drop-shadow-base bg-white rounded-custom lg:px-8">
+        {{-- Technical Specifications --}}
+        <section class="default_container mb-20">
+            <p
+                class="pr-4 relative text-xl mb-7 font-bold text-stone-700 before:top-1/2 before:right-0 before:-translate-y-1/2 before:bg-normal before:h-12 before:w-1 before:rounded-l-custom before:absolute before:content-['']">
+                مشخصات </p>
+            <ul class="flex flex-col gap-2 text-stone-700">
+                @foreach ($product->attributes->sortBy('parent_id')->groupBy('parent_id') as $key => $attrs)
+                    <x-splade-data default="{ toggle: {{ $loop->index == 0 ? 'true' : 'false' }} }">
+                        <li class="p-4 bg-stone-200 rounded-custom lg:px-8">
                             {{-- title --}}
-                            <p class="text-sm lg:text-base font-bold"> بررسی اجمالی </p>
+                            <div class="flex items-center justify-between cursor-pointer"
+                                @click="data.toggle = !data.toggle">
+                                <p class="text-sm lg:text-base font-bold">
+                                    {{ \App\Models\LandAttribute::whereId($key)->first()->name }} </p>
+                                <button type="button" class="cursor-pointer">
+                                    <svg class="duration-1000" :class="data.toggle ? 'rotate-180' : 'rotate-0'"
+                                        width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M7 10L12.0008 14.58L17 10" stroke="#111827" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <ul class="list-inside list-disc marker:text-stone-700 text-sm lg:text-base font-normal flex flex-col gap-2 duration-1000 overflow-hidden lg:pr-2"
+                                v-bind:class="data.toggle ? 'max-h-96 pb-4 mt-4' : 'max-h-0 pb-0'">
+                                @foreach ($attrs as $attr)
+                                    <li> {{ $attr->name }} : {{ $attr->pivot->value->value }} </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    </x-splade-data>
+                @endforeach
+            </ul>
+        </section>
 
-                            <div class="lg:pr-2 pb-4 mt-4">
-                                <div class="text-sm !leading-8 lg:text-base font-normal pb-4 text-justify">
-                                    {!! $product->body !!}
+        {{-- videos --}}
+        <section class="mb-20 relative default_container" id="video-player-container">
+            <p
+                class="pr-4 relative text-xl mb-7 font-bold text-stone-700 before:top-1/2 before:right-0 before:-translate-y-1/2 before:bg-normal before:h-12 before:w-1 before:rounded-l-custom before:absolute before:content-['']">
+                ویدیو </p>
+            <x-home_landing.videos :data="$product->videos" />
+        </section>
+
+        {{-- video modal --}}
+        <section class="fixed inset-0 z-[4] bg-black/60 hidden" id="ifame-container"
+            onclick="hideVideoByThumbnail(this)">
+            <div class="w-full max-w-[640px] lg:max-w-[796px] xl:max-w-[900px] 2xl:max-w-[1024px] mx-auto iframe_styles flex_center"
+                id="ifame-box"></div>
+        </section>
+
+        {{-- viewpoint --}}
+        <section class="default_container mb-20">
+            <p
+                class="pr-4 relative text-xl mb-7 font-bold text-stone-700 before:top-1/2 before:right-0 before:-translate-y-1/2 before:bg-normal before:h-12 before:w-1 before:rounded-l-custom before:absolute before:content-['']">
+                دیدگاه </p>
+            <ul class="flex flex-col gap-7 text-gray-900">
+                @if ($comments->count() > 0)
+                    @foreach ($comments as $comment)
+                        <li>
+                            {{-- user name --}}
+                            <p class="block text-base font-bold text-stone-700 mb-1.5"> {{ $comment->name }} </p>
+
+                            {{-- user viewpoint --}}
+                            <div class="flex flex-col gap-4 bg-stone-700/5 rounded-custom p-4">
+                                <div class="font-normal leading-6 lg:leading-7 text-justify text-sm lg:text-base">
+                                    {{ $comment->comment }}
+                                </div>
+
+                                <div class="flex items-center text-sm font-normal justify-end gap-5 text-stone-700">
+                                    {{-- like --}}
+                                    <button type="button" class="flex items-center gap-2">
+                                        <svg class="h-5 stroke-current" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M1.79169 8.9668H5.82198V19.6982H1.79341C1.6816 19.6985 1.57085 19.6766 1.46749 19.634C1.36413 19.5914 1.27019 19.5288 1.19105 19.4498C1.11192 19.3708 1.04913 19.277 1.00629 19.1737C0.963456 19.0705 0.941406 18.9597 0.941406 18.8479V9.81708C0.941406 9.59157 1.03099 9.3753 1.19045 9.21584C1.34991 9.05638 1.56618 8.9668 1.79169 8.9668Z" stroke="current" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M5.83746 19.7478L7.06317 20.3598C7.53803 20.5964 8.06088 20.7181 8.59231 20.7164H19.7506C20.2231 20.7158 20.6792 20.5427 21.033 20.2295C21.3869 19.9164 21.6142 19.4848 21.6723 19.0158L23.0317 10.2404C23.0731 9.96737 23.056 9.68869 22.9818 9.42272C22.9076 9.15674 22.7779 8.90952 22.6012 8.69733C22.4244 8.48515 22.2048 8.31282 21.9566 8.19171C21.7085 8.07059 21.4374 8.00344 21.1615 7.99468H13.694V3.04554C13.6917 2.79354 13.6389 2.54457 13.5387 2.31331C13.4386 2.08204 13.2932 1.87317 13.111 1.69902C12.9289 1.52486 12.7137 1.38894 12.4782 1.29928C12.2427 1.20962 11.9916 1.16803 11.7397 1.17697C11.4194 1.1736 11.1037 1.25332 10.8234 1.40835C10.5431 1.56338 10.3077 1.78843 10.1403 2.06154L5.82031 8.96668V19.6981" stroke="current" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                        <p> 0 </p>
+                                    </button>
+                                    {{-- dislike --}}
+                                    <button type="button" class="flex items-center gap-2">
+                                        <svg class="h-5 stroke-current" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M1.79169 13.0332H5.82198V2.30177H1.79341C1.6816 2.30155 1.57085 2.32338 1.46749 2.36601C1.36413 2.40864 1.27019 2.47123 1.19105 2.55021C1.11192 2.62919 1.04913 2.723 1.00629 2.82627C0.963456 2.92955 0.941406 3.04025 0.941406 3.15206V12.1829C0.941406 12.4084 1.03099 12.6247 1.19045 12.7842C1.34991 12.9436 1.56618 13.0332 1.79169 13.0332Z" stroke="current" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M5.83746 2.25218L7.06317 1.64017C7.53803 1.4036 8.06088 1.28189 8.59231 1.2836H19.7506C20.2231 1.2842 20.6792 1.45734 21.033 1.77048C21.3869 2.08363 21.6142 2.51523 21.6723 2.98418L23.0317 11.7596C23.0731 12.0326 23.056 12.3113 22.9818 12.5773C22.9076 12.8433 22.7779 13.0905 22.6012 13.3027C22.4244 13.5149 22.2048 13.6872 21.9566 13.8083C21.7085 13.9294 21.4374 13.9966 21.1615 14.0053H13.694V18.9545C13.6917 19.2065 13.6389 19.4554 13.5387 19.6867C13.4386 19.918 13.2932 20.1268 13.111 20.301C12.9289 20.4751 12.7137 20.6111 12.4782 20.7007C12.2427 20.7904 11.9916 20.832 11.7397 20.823C11.4194 20.8264 11.1037 20.7467 10.8234 20.5916C10.5431 20.4366 10.3077 20.2116 10.1403 19.9385L5.82031 13.0333V2.30189" stroke="current" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                        <p> 0 </p>
+                                    </button>
                                 </div>
                             </div>
                         </li>
-                    </ul>
-                </section>
-
-                {{-- Technical Specifications --}}
-                <section class="default_container" v-show="data.activeTab == 2">
-                    <ul class="flex flex-col gap-2 text-gray-900">
-                        @foreach($product->attributes->sortBy('parent_id')->groupBy('parent_id') as $key => $attrs)
-                            <x-splade-data default="{ toggle: {{ $loop->index == 0 ? 'true' : 'false' }} }">
-                                <li class="p-4 drop-shadow-base bg-white rounded-custom lg:px-8">
-                                    {{-- title --}}
-                                    <div class="flex items-center justify-between cursor-pointer"
-                                         @click="data.toggle = !data.toggle">
-                                        <p class="text-sm lg:text-base font-bold"> {{ \App\Models\LandAttribute::whereId($key)->first()->name }} </p>
-                                        <button type="button" class="cursor-pointer">
-                                            <svg class="duration-1000" :class="data.toggle ? 'rotate-180' : 'rotate-0'"
-                                                 width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                 xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M7 10L12.0008 14.58L17 10" stroke="#111827" stroke-width="2"
-                                                      stroke-linecap="round" stroke-linejoin="round"/>
-                                            </svg>
-                                        </button>
-                                    </div>
-                                    <ul class="list-inside list-disc marker:text-gray-900 text-sm lg:text-base font-normal flex flex-col gap-2 duration-1000 overflow-hidden lg:pr-2"
-                                        v-bind:class="data.toggle ? 'max-h-96 pb-4 mt-4' : 'max-h-0 pb-0'">
-                                        @foreach($attrs as $attr)
-                                            <li> {{ $attr->name }} : {{ $attr->pivot->value->value }} </li>
-                                        @endforeach
-                                    </ul>
-                                </li>
-                            </x-splade-data>
-                        @endforeach
-                    </ul>
-                </section>
-
-                {{-- viewpoint --}}
-                <section class="default_container" v-show="data.activeTab == 3">
-                    <ul class="flex flex-col gap-7 text-gray-900 mb-4">
-                        @if($comments->count() > 0)
-                            @foreach($comments as $comment)
-                                <x-splade-data default="{ toggle: false }">
-                                    <li class="p-4 drop-shadow-base bg-white rounded-custom lg:px-8">
-                                        {{-- user name --}}
-                                        <p class="block text-sm leading-6 lg:leading-7 lg:text-base font-bold text-[#585d68] border-b border-[#e7e8e9] mb-3 pb-0.5">
-                                            {{ $comment->name }} </p>
-                                        {{-- user viewpoint --}}
-                                        <div class="flex justify-between items-start gap-10 sm:px-4">
-                                            <p class="font-normal leading-6 lg:leading-7 text-justify text-sm lg:text-base">
-                                                {{ $comment->comment }}
-                                            </p>
-                                            <button type="button" class="cursor-pointer mt-1.5"
-                                                    @click="data.toggle = !data.toggle">
-                                                <svg class="duration-1000" :class="data.toggle ? 'rotate-180' : 'rotate-0'"
-                                                    width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                    xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M7 10L12.0008 14.58L17 10" stroke="#111827" stroke-width="2"
-                                                        stroke-linecap="round" stroke-linejoin="round"/>
-                                                </svg>
-                                            </button>
-                                        </div>
-
-                                        <div class="duration-1000 overflow-hidden bg-gray-100 px-2.5 sm:pr-8 rounded-custom"
-                                            v-bind:class="data.toggle ? 'max-h-96 py-4 sm:py-5 mt-4' : 'max-h-0 pb-0'">
-                                            <div class="flex items-center gap-4 mb-2">
-                                                <svg width="14" height="14" viewBox="0 0 14 14" fill="none"
-                                                    xmlns="http://www.w3.org/2000/svg">
-                                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                                        d="M7 11.993C10.866 11.993 14 10.029 14 6.10099C14 2.17299 10.866 0.208984 7 0.208984C3.134 0.208984 0 2.17299 0 6.10099C0 8.22211 0.914 9.77072 2.364 10.7459L2.454 12.868C2.46286 13.0754 2.52945 13.2766 2.64655 13.4495C2.76366 13.6224 2.92678 13.7605 3.11821 13.8488C3.30963 13.9371 3.52203 13.9721 3.73232 13.9502C3.94262 13.9282 4.14276 13.8501 4.311 13.7243L6.633 11.9861C6.755 11.991 6.877 11.993 7 11.993Z"
-                                                        fill="#5289FF"/>
-                                                </svg>
-                                                <p class="font-normal text-sm leading-6 lg:text-base lg:leading-7"> پاسخ : </p>
-                                            </div>
-                                            <p class="text-sm leading-6 lg:text-base lg:leading-7 text-justify font-normal pr-4 sm:pr-6">
-                                                کامیونت 8.5 تن جک یا N721-N85 از تولیدات شرکت جک موتورز چین است که توسط آرین
-                                                دیزل مونتاژ و به بازار عرضه می‌شود. این خودرو با داشتن قدرت موتور 155 اسب بخار و
-                                                تناژ 8.5 تن برای مسیرهای بین شهری مناسب است. در طی سال‌های اخیر، کامیونت‌های جک
-                                                مورد توجه بخش بزرگی از بازار قرار گرفته زیرا این خودروها دارای قدرت بالا و
-                                                استهلاک کمی هستند. کامیونت 8.5 تن جک با داشتن جای خواب و صندلی دونفره شاگرد،
-                                                مسیر سفر را برای سرنشینان خود هموار می‌کند. برای اطلاعات بیشتر در ادامه با ما
-                                                همراه باشید.
-                                            </p>
-                                        </div>
-                                    </li>
-                                </x-splade-data>
-                            @endforeach
-                        @else
-                            <li class="h-44 w-full flex_center text-center text-normal leading-6 lg:leading-7 text-sm lg:text-base">
-                                شما می توانید نظر خود را با ما در میان بگذارید.
-                            </li>
-                        @endif
-                    </ul>
-                    @if($comments->count() > 3)
-                        <button type="button"
-                                class="mx-auto text-normal text-sm font-bold lg:text-base cursor-pointer block"> مشاهده همه
-                            دیدگاه ها
-                        </button>
-                    @endif
-                </section>
-            </section>
-        </x-splade-data>
-
-        <x-home_landing.videos
-            showAllBtn="false"
-            :data="$product->videos"
-            />
+                    @endforeach
+                @else
+                    <li
+                        class="h-44 w-full flex_center text-center text-normal leading-6 lg:leading-7 text-sm lg:text-base">
+                        شما می توانید نظر خود را با ما در میان بگذارید.
+                    </li>
+                @endif
+            </ul>
+            @if ($comments->count() > 3)
+                <button type="button" class="mx-auto text-normal mt-10 text-sm font-bold lg:text-base cursor-pointer block">
+                    مشاهده همه
+                    دیدگاه ها
+                </button>
+            @endif
+        </section>
 
         {{-- add viewpoint --}}
         <x-pdp_landing.addViewpoint :land="$land" :product="$product" />
