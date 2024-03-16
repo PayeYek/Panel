be46b65639772a0cd94899ecabe0e35790e65a37
themestermim <themestermim@gmail.com>
2024-03-16 11:11:31 +0330

    slider if empty infos bug finished.

--- resources/js/components/Slider.vue
+++ resources/js/components/Slider.vue
@@ -1,18 +1,23 @@
 <template>
+    <!-- :autoplay="{delay: 3500, disableOnInteraction: false}" -->
     <section class="mb-6 lg:mb-8 default_container">
         <swiper
             :modules="modules"
             :slides-per-view="1"
             :navigation="true"
-            :autoplay="{delay: 3500, disableOnInteraction: false}"
+            
             :pagination="{ clickable: true }"
             class="land_slider slider_type_1"
         >
             <swiper-slide v-for="(slide, index) in slides" :key="index">
                 <a href="#" class="relative w-full pt-[44%] block rounded-b-custom overflow-hidden">
                     <img class="object-cover w-full h-full top-0 left-0 absolute" :src="slide.image" :alt="slide.alt">
-                    <ul class="list-disc list-inside absolute top-2.5 right-1 py-1 px-2 z-[1] bg-white/80 text-xs rounded-custom">
-                        <li v-for="(li, innerIndex) in slide.att" :key="innerIndex" v-if="li !== ''" :class="{ 'line-clamp-1': true, 'hidden': innerIndex > 1, 'sm:block': innerIndex > 1, 'lg:block': innerIndex > 3 }" class="line-clamp-1" v-text="slide.alt"></li>
+                    <ul class="list-disc list-inside absolute top-2.5 sm:top-4 sm:right-2 right-1 py-1 px-2 sm:px-4 sm:py-2 space-y-1.5 z-[1] bg-white/80 text-xs font-bold text-stone-700 rounded-custom">
+                        <template v-for="(li, innerIndex) in slide.infos" :key="innerIndex">
+                            <li class="marker:text-stone-700 sm:text-sm md:text-base" v-if="li" :class="{ 'hidden': innerIndex > 0, 'sm:list-item': innerIndex >= 1 && innerIndex < 3, 'md:list-item': innerIndex >= 3 }">
+                                <p class="truncate inline-flex"> {{ li }} </p>
+                            </li>
+                        </template>
                     </ul>
                 </a>
             </swiper-slide>
@@ -47,7 +52,10 @@ export default {
             //console.log('slide change');
         };
 
-        console.log();
+        // props.slides.map(item => {
+        //     console.log(item.infos[1]);
+            
+        // })
         return {
             onSwiper,
             onSlideChange,
