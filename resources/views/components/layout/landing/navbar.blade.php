e9e652bacf88651dd829ef163dca78140dd1fc58
themestermim <themestermim@gmail.com>
2024-03-17 12:45:18 +0330

    finished.

--- resources/views/components/layout/landing/navbar.blade.php
+++ resources/views/components/layout/landing/navbar.blade.php
@@ -7,6 +7,6 @@
     <Link class="focus:bg-[#eaeaea] lg:hover:text-stone-900 lg:focus:bg-transparent font-medium lg:first:pr-0 lg:last:pl-0 lg:last:border-l-0" href="{{ route('landing.page.show', ['page'=>$land->slug]) }}">{{__('Home')}}</Link>
     <Link class="focus:bg-[#eaeaea] lg:hover:text-stone-900 lg:focus:bg-transparent font-medium lg:first:pr-0 lg:last:pl-0 lg:last:border-l-0" href="{{ route('landing.product.list', ['page' => $land->slug]) }}">{{__('Products')}}</Link>
     <Link class="focus:bg-[#eaeaea] lg:hover:text-stone-900 lg:focus:bg-transparent font-medium lg:first:pr-0 lg:last:pl-0 lg:last:border-l-0" href="{{ route('landing.sales', ['page' => $land->slug]) }}">{{__('Sales Agency')}}</Link>
-    <Link class="focus:bg-[#eaeaea] lg:hover:text-stone-900 lg:focus:bg-transparent font-medium lg:first:pr-0 lg:last:pl-0 lg:last:border-l-0" href="{{ route('landing.article.list', ['page' => $land->slug]) }}">{{__('Notify')}}</Link>
+    <Link class="focus:bg-[#eaeaea] lg:hover:text-stone-900 lg:focus:bg-transparent font-medium lg:first:pr-0 lg:last:pl-0 lg:last:border-l-0" href="{{ route('landing.article.list', ['page' => $land->slug, 'f' => 'sell']) }}">{{__('Notify')}}</Link>
     <Link class="focus:bg-[#eaeaea] lg:hover:text-stone-900 lg:focus:bg-transparent font-medium lg:first:pr-0 lg:last:pl-0 lg:last:border-l-0" href="{{ route('landing.page.about', ['page'=>$land->slug]) }}">{{__('About us')}}</Link>
 </nav>
