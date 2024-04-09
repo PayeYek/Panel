7a7bcc7da5dbfe6fd8bb2b734c1fe04e3e3424ca
themestermim <themestermim@gmail.com>
2024-04-09 14:20:44 +0330

    calculator static finished.

--- resources/js/components/computing/index.vue
+++ resources/js/components/computing/index.vue
@@ -27,13 +27,12 @@
                         <div class="h-2 w-full bg-stone-200 rounded-full mb-4 relative">
                             <input
                                 :style="{ background: priceSliderBackground }"
-                                type="range" class="dir-rtl absolute top-0.5 inset-x-0.5 range__input"
+                                type="range" class="dir-rtl absolute top-0.5 inset-x-0.5 range__input rounded-full"
                                 :min="loanMin"
                                 :max="loanMax"
                                 :value="loanInitialValue"
                                 v-model="loanInitialValue"
-                                :step="loanSteps"
-                                @change="calculateDeposite" />
+                                :step="loanSteps" />
                         </div>
                         <div class="text-sm font-normal flex items-center justify-between mb-4 text-stone-700">
                             <p> 200 میلیون تومان </p>
@@ -41,28 +40,51 @@
                         </div>
                     </section>
         
-                    <!-- discount percent -->
-                    <section class="mb-8 lg:mb-14">
-                        <p class="text-sm pr-4 font-medium text-stone-700 mb-4 relative before:absolute before:content-[''] before:rounded-full before:bg-normal before:top-1.5 before:right-0 before:size-2"> نوع خودرو </p>
+                    <!-- payment duration -->
+                    <section>
+                        <p class="text-sm pr-4 font-medium text-stone-700 mb-4 relative before:absolute before:content-[''] before:rounded-full before:bg-normal before:top-1.5 before:right-0 before:size-2"> مدت زمان بازپرداخت <span class="sm:hidden text-stone-700 text-xs font-medium"> (ماهه) </span> </p>
         
-                        <section class="flex_center lg:gap-4 gap-2 text-stone-700 text-sm font-normal">
+                        <section class="flex_center gap-2 text-stone-700 text-sm font-normal">
                             <div>
-                                <input type="radio" value="internal" v-model="manufacture" name="manufacture" id="internal" class="hidden peer" />
-                                <label for="internal" class="h-12 rounded-custom w-20 peer-checked:w-28 sm:w-32 sm:peer-checked:w-40 cursor-pointer lg:w-32 lg:peer-checked:w-40 border flex_center border-stone-400 bg-white peer-checked:border-[#599CFF] peer-checked:bg-[#EEF5FF] peer-checked:font-medium"> داخلی </label>
+                                <input type="radio" value="12" v-model="paymentDuration" name="paymentDuration" id="12monthes" class="hidden peer" />
+                                <label for="12monthes" class="h-12 rounded-custom w-12 peer-checked:w-16 sm:w-24 sm:peer-checked:w-28 sm:gap-1 cursor-pointer lg:w-20 lg:peer-checked:w-32 ring-1 flex_center ring-stone-400 bg-white peer-checked:ring-stone-950 peer-checked:ring-2 peer-checked:bg-stone-200 peer-checked:font-medium">
+                                    <span> 12 </span>
+                                    <span class="hidden sm:block"> ماهه </span>
+                                </label>
                             </div>
                             <div>
-                                <input type="radio" value="chinese" v-model="manufacture" name="manufacture" id="chinese" class="hidden peer" />
-                                <label for="chinese" class="h-12 rounded-custom w-20 peer-checked:w-28 sm:w-32 sm:peer-checked:w-40 cursor-pointer lg:w-32 lg:peer-checked:w-40 border flex_center border-stone-400 bg-white peer-checked:border-[#599CFF] peer-checked:bg-[#EEF5FF] peer-checked:font-medium"> چینی </label>
+                                <input type="radio" value="24" v-model="paymentDuration" name="paymentDuration" id="24monthes" class="hidden peer" />
+                                <label for="24monthes" class="h-12 rounded-custom w-12 peer-checked:w-16 sm:w-24 sm:peer-checked:w-28 sm:gap-1 cursor-pointer lg:w-20 lg:peer-checked:w-32 ring-1 flex_center ring-stone-400 bg-white peer-checked:ring-stone-950 peer-checked:ring-2 peer-checked:bg-stone-200 peer-checked:font-medium">
+                                    <span> 24 </span>
+                                    <span class="hidden sm:block"> ماهه </span>
+                                </label>
                             </div>
                             <div>
-                                <input type="radio" value="european" v-model="manufacture" name="manufacture" id="European" class="hidden peer" />
-                                <label for="European" class="h-12 rounded-custom w-20 peer-checked:w-28 sm:w-32 sm:peer-checked:w-40 cursor-pointer lg:w-32 lg:peer-checked:w-40 border flex_center border-stone-400 bg-white peer-checked:border-[#599CFF] peer-checked:bg-[#EEF5FF] peer-checked:font-medium"> اروپایی </label>
+                                <input type="radio" value="36" v-model="paymentDuration" name="paymentDuration" id="36monthes" class="hidden peer" />
+                                <label for="36monthes" class="h-12 rounded-custom w-12 peer-checked:w-16 sm:w-24 sm:peer-checked:w-28 sm:gap-1 cursor-pointer lg:w-20 lg:peer-checked:w-32 ring-1 flex_center ring-stone-400 bg-white peer-checked:ring-stone-950 peer-checked:ring-2 peer-checked:bg-stone-200 peer-checked:font-medium">
+                                    <span> 36 </span>
+                                    <span class="hidden sm:block"> ماهه </span>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="radio" value="48" v-model="paymentDuration" name="paymentDuration" id="48monthes" class="hidden peer" />
+                                <label for="48monthes" class="h-12 rounded-custom w-12 peer-checked:w-16 sm:w-24 sm:peer-checked:w-28 sm:gap-1 cursor-pointer lg:w-20 lg:peer-checked:w-32 ring-1 flex_center ring-stone-400 bg-white peer-checked:ring-stone-950 peer-checked:ring-2 peer-checked:bg-stone-200 peer-checked:font-medium">
+                                    <span> 48 </span>
+                                    <span class="hidden sm:block"> ماهه </span>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="radio" value="60" v-model="paymentDuration" name="paymentDuration" id="60monthes" class="hidden peer" />
+                                <label for="60monthes" class="h-12 rounded-custom w-12 peer-checked:w-16 sm:w-24 sm:peer-checked:w-28 sm:gap-1 cursor-pointer lg:w-20 lg:peer-checked:w-32 ring-1 flex_center ring-stone-400 bg-white peer-checked:ring-stone-950 peer-checked:ring-2 peer-checked:bg-stone-200 peer-checked:font-medium">
+                                    <span> 60 </span>
+                                    <span class="hidden sm:block"> ماهه </span>
+                                </label>
                             </div>
                         </section>
                     </section>
         
                     <!-- monthes -->
-                    <section class="mb-8 lg:mb-14 text-stone-700">
+                    <!-- <section class="mb-8 lg:mb-14 text-stone-700">
                         <p class="text-sm pr-4 font-medium mb-4 relative before:absolute before:content-[''] before:rounded-full before:bg-normal before:top-1.5 before:right-0 before:size-2"> مدت زمان بازگشت وجه </p>
                         <select class="w-full h-12 rounded-custom bg-stone-200 border-0 focus:ring-0 outline-none !pr-6 font-normal text-sm" v-model="initialMonth">
                             <option value="12"> 12 قسط </option>
@@ -71,43 +93,44 @@
                             <option value="48"> 48 قسط </option>
                             <option value="60"> 60 قسط </option>
                         </select>
-                    </section>
+                    </section> -->
         
                     <!-- calculate -->
-                    <button class="text-lg font-normal mx-auto bg-normal cursor-pointer rounded-custom text-white flex_center h-12 w-52" type="button"> محاسبه کن </button>
+                    <!-- <button class="text-lg font-normal mx-auto bg-normal cursor-pointer rounded-custom text-white flex_center h-12 w-52" type="button"> محاسبه کن </button> -->
                 </section>
             </section>
     
             <!-- total -->
             <section class="lg:col-span-4">
-                <section class="bg-[#EEF5FF] text-sm font-normal rounded-custom px-4 py-6 lg:py-14 lg:px-10">
-                    <p class="text-lg font-medium text-stone-700 mb-4 lg:mb-6"> نتیجه محاسبه </p>
-                    <div class="flex_between border bg-white border-stone-400 rounded-custom w-full h-12 pr-2 pl-6 mb-2 lg:mb-4">
-                        <p class="text-stone-500"> مبلغ هر قسط: </p>
-                        <p class="text-stone-700"> تومان </p>
+                <section class="bg-stone-200 text-sm font-normal rounded-b-custom px-4 py-6 lg:py-12 lg:px-10">
+                    <div class="flex_between mb-4 lg:mb-6">
+                        <p class="text-base lg:text-lg font-medium text-stone-700"> نتیجه محاسبه </p>
+                        <p class="text-sm text-[#1EA0FF] border-b-2 border-b-transparent hover:border-b-[#1EA0FF] border-dashed font-medium cursor-pointer"> اطلاعات بیشتر </p>
                     </div>
-                    <div class="flex_between border bg-white border-stone-400 rounded-custom w-full h-12 pr-2 pl-6 mb-2 lg:mb-4">
-                        <p class="text-stone-500"> وثیقه: </p>
-                        <p class="text-stone-700"> تومان </p>
-                    </div>
-                    <div class="flex_between border bg-white border-stone-400 rounded-custom w-full h-12 pr-2 pl-6 mb-2 lg:mb-4">
-                        <p class="text-stone-500"> نرخ موثر: </p>
-                        <p class="text-stone-700"> تومان </p>
-                    </div>
-                    <div class="flex_between border bg-white border-stone-400 rounded-custom w-full h-12 pr-2 pl-6 mb-8 lg:mb-12">
-                        <p class="text-stone-500"> هزینه عملیات: </p>
-                        <p class="text-stone-700"> تومان </p>
-                    </div>
-                    <ul class="list-disc list-inside text-stone-700 text-sm font-normal mb-4">
-                        <li class="mb-2 last:mb-0"> مبلغ تسهیلات 40 تا 60 درصد ارزش وسیله نقلیه می باشد. </li>
-                        <li class="mb-2 last:mb-0"> هزینه عملیات بدون ارزش افزوده محاسبه گردیده است. </li>
-                        <li class="mb-2 last:mb-0"> سود اقساط شما معادل نرخ مصوب بانک مرکزی یعنی 23 درصد است. </li>
+                    <ul class="list-none text-[#90A4AE] mb-6">
+                        <li class="border-b border-b-white py-4 flex_between gap-4 text-sm font-normal">
+                            <p class=""> مبلغ خالص تسهیلات دریافتی </p>
+                            <div class="flex_row gap-2 sm:gap-4">
+                                <p class="text-stone-950"> 0000 </p>
+                                <p class=""> تومان </p>
+                            </div>
+                        </li>
+                        <li class="border-b border-b-white py-4 flex_between gap-4 text-sm font-normal">
+                            <p class=""> مبلغ هر قسط (فواصل دو ماهه) </p>
+                            <div class="flex_row gap-2 sm:gap-4">
+                                <p class="text-stone-950"> 0000 </p>
+                                <p class=""> تومان </p>
+                            </div>
+                        </li>
+                        <li class="border-b border-b-white py-4 flex_between gap-4 text-sm font-normal">
+                            <p class=""> مبلغ کل بازپرداخت </p>
+                            <div class="flex_row gap-2 sm:gap-4">
+                                <p class="text-stone-950"> 0000 </p>
+                                <p class=""> تومان </p>
+                            </div>
+                        </li>
                     </ul>
-                    <form action="#" class="border-2 border-white rounded-custom py-4 sm:py-6 sm:px-12 px-6 flex flex-col items-center">
-                        <p class="text-center text-sm font-medium text-normal mb-2 lg:mb-4"> جهت مشاوره و خرید شماره خود را وارد کنید: </p>
-                        <input type="tel" class="w-full max-w-64 mx-auto h-12 rounded-custom bg-white border border-stone-400 focus:border-stone-400 mb-4 tracking-widest focus:ring-0 outline-none placeholder:text-[#8A8B8C] px-3" placeholder="0912" />
-                        <button class="w-full max-w-64 mx-auto h-12 rounded-custom border font-medium text-base border-normal text-normal hover:bg-normal hover:text-white duration-200" type="submit"> ثبت </button>
-                    </form>
+                    <button class="text-lg font-medium mx-auto bg-normal cursor-pointer rounded-custom text-white flex_center h-12 px-10 lg:px-14 xl:px-20" type="button"> درخواست مشاوره </button>
                 </section>
             </section>
         </section>
@@ -130,41 +153,23 @@ export default {
         const loanMin = ref(200000000);
         const loanMax = ref(3000000000);
         const loanInitialValue = ref(400000000);
-        const manufacture = ref("internal");
-        const initialMonth = ref(24);
-        // const monthlySteps = ref(1);
-        // const monthlyMin = ref(1);
-        // const monthlyMax = ref(36);
-        // const monthlyInitialValue = ref(6);
-        // const loanInitalPerMonth = ref(0);
-        // const loanInitalTotal = ref(0);
-
-        // console.log(numberWithCommas(1523654));
+        const paymentDuration = ref(24);
+        
 
         const generatePriceBackground = (value) => {
             let percentage = (value - loanMin.value) / (loanMax.value - loanMin.value) * 100;
-            return `linear-gradient(to left, #599CFF ${percentage}%, transparent ${percentage}%)`;
+            return `linear-gradient(to left, #1A1B1D ${percentage}%, transparent ${percentage}%)`;
         };
 
-        // const generateMonthBackground = (value) => {
-        //     let percentage = (value - monthlyMin.value) / (monthlyMax.value - monthlyMin.value) * 100;
-        //     return `linear-gradient(to left, #878787 ${percentage}%, #d2d2d2 ${percentage}%)`;
-        // };
-
         const priceSliderBackground = computed(() => generatePriceBackground(loanInitialValue.value));
-        // const monthSliderBackground = computed(() => generateMonthBackground(monthlyInitialValue.value));
 
         const loanOrder = (payload) => {
-            console.log(typeof loanInitialValue.value, typeof loanMax.value, typeof payload, typeof loanSteps.value);
             if (loanInitialValue.value < loanMax.value && payload === 'plus') {
-                loanInitialValue.value += loanSteps.value
+                loanInitialValue.value = parseInt(loanInitialValue.value);
+                loanInitialValue.value += loanSteps.value;
             } else if(loanInitialValue.value > loanMin.value && payload === 'minus') {
-                loanInitialValue.value -= loanSteps.value
+                loanInitialValue.value -= loanSteps.value;
             }
-            console.log(numberWithCommas(loanInitialValue.value));
-        }
-
-        const calculateDeposite = (e) => {
         }
 
         return {
@@ -173,19 +178,10 @@ export default {
             loanMin,
             loanMax,
             loanInitialValue,
-            manufacture,
-            initialMonth,
+            paymentDuration,
             numberWithCommas,
             loanOrder,
-            // monthlySteps,
-            // monthlyMin,
-            // monthlyMax,
-            // monthlyInitialValue,
-            calculateDeposite,
-            // loanInitalPerMonth,
-            // loanInitalTotal,
             priceSliderBackground,
-            // monthSliderBackground,
         }
     }
 }
