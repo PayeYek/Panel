b5f786fcf5a6dcf28f60be21bb8ee2057a96a6cb
Amin Sheykhi <isheykhi@gmail.com>
2024-04-13 15:13:34 +0330

    Remove Icons from Splash vendor

--- resources/views/vendor/splade/table/bulk-actions-exports.blade.php
+++ resources/views/vendor/splade/table/bulk-actions-exports.blade.php
@@ -1,18 +1,25 @@
 <x-splade-component is="button-with-dropdown" dusk="bulk-actions-exports-dropdown" v-bind:close-on-click="true">
     <x-slot:button>
-            <x-iconsax-bol-flash-1 class="w-5 h-5 text-gray-400 dark:text-gray-400 group-hover:text-primary-700 dark:group-hover:text-white"/>
-            <span class="hidden sm:block">{{ __('Actions') }}</span>
+        {{--Flash--}}
+        <svg class="w-5 h-5 text-gray-400 dark:text-gray-400 group-hover:text-primary-700 dark:group-hover:text-white"
+             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+            <path
+                d="M17.91 10.72h-3.09v-7.2c0-1.68-.91-2.02-2.02-.76l-.8.91-6.77 7.7c-.93 1.05-.54 1.91.86 1.91h3.09v7.2c0 1.68.91 2.02 2.02.76l.8-.91 6.77-7.7c.93-1.05.54-1.91-.86-1.91z"></path>
+        </svg>
+        <span class="hidden sm:block">{{ __('Actions') }}</span>
     </x-slot:button>
 
-    <div class="min-w-max z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 block overflow-hidden">
+    <div
+        class="min-w-max z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 block overflow-hidden">
         <div class="flex flex-col">
-            <h3 v-if="table.hasSelectedItems" class="p-3 text-xs font-medium text-gray-900 dark:text-white uppercase tracking-wide">
+            <h3 v-if="table.hasSelectedItems"
+                class="p-3 text-xs font-medium text-gray-900 dark:text-white uppercase tracking-wide">
                 <span v-if="table.totalSelectedItems == 1">
-                    <span v-text="table.totalSelectedItems" /> {{ __('Item selected') }}
+                    <span v-text="table.totalSelectedItems"/> {{ __('Item selected') }}
                 </span>
 
                 <span v-if="table.totalSelectedItems > 1">
-                    <span v-text="table.totalSelectedItems" /> {{ __('Items selected') }}
+                    <span v-text="table.totalSelectedItems"/> {{ __('Items selected') }}
                 </span>
             </h3>
 
--- resources/views/vendor/splade/table/controls.blade.php
+++ resources/views/vendor/splade/table/controls.blade.php
@@ -32,8 +32,9 @@
                 @click.prevent="table.reset"
                 dusk="reset-table"
                 class="group w-full lg:w-auto flex space-x-1 rtl:space-x-reverse items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-            <x-iconsax-bol-close-circle
-                class="w-5 h-5 text-gray-400 dark:text-gray-400 group-hover:text-primary-700 dark:group-hover:text-white"/>
+            <svg class="w-5 h-5 text-gray-400 dark:text-gray-400 group-hover:text-primary-700 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                <path d="M12 2C6.49 2 2 6.49 2 12s4.49 10 10 10 10-4.49 10-10S17.51 2 12 2zm3.36 12.3c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22s-.38-.07-.53-.22l-2.3-2.3-2.3 2.3c-.15.15-.34.22-.53.22s-.38-.07-.53-.22a.754.754 0 010-1.06l2.3-2.3-2.3-2.3a.754.754 0 010-1.06c.29-.29.77-.29 1.06 0l2.3 2.3 2.3-2.3c.29-.29.77-.29 1.06 0 .29.29.29.77 0 1.06l-2.3 2.3 2.3 2.3z"></path>
+            </svg>
             <span>{{ __('Reset') }}</span>
         </button>
     </div>
