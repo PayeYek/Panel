0b11a12bfe3d48be196b02188420a69da3cef262
Gharaei <ali.gharaei360@gmail.com>
2024-05-02 12:59:44 +0330

    [Fix]

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,23 +13,23 @@ return new class extends Migration {
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-//            $table->id();
-//            $table->string('first_name', 64)->nullable();
-//            $table->string('last_name', 64)->nullable();
-//            $table->tinyInteger('gender')->nullable();
-//            $table->string('email')->nullable();
-//            $table->string('phone', 10);
-//            $table->date('birthdate')->nullable();
-//            $table->tinyInteger('type')->nullable();
-//            $table->string('ssn', 10)->nullable();
-//            $table->timestamp('email_verified_at')->nullable();
-//            $table->tinyInteger('certified')->nullable();
-//            $table->tinyInteger('state')->nullable();
-//
-//            $table->rememberToken();
-//            $table->timestamps();
-//
-//            $table->unique(['phone', 'ssn']);
+            $table->id();
+            $table->string('first_name', 64)->nullable();
+            $table->string('last_name', 64)->nullable();
+            $table->tinyInteger('gender')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone', 10);
+            $table->date('birthdate')->nullable();
+            $table->tinyInteger('type')->nullable();
+            $table->string('ssn', 10)->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->tinyInteger('certified')->nullable(); //Todo implement Enum
+            $table->tinyInteger('state')->nullable(); //Todo implement Enum\
+
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->unique(['phone', 'ssn']);
         });
     }
 
