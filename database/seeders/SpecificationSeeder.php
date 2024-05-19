b197751a0ad1c400e10ee8af67179e723df276c0
Gharaei <ali.gharaei360@gmail.com>
2024-05-19 17:24:43 +0330

    [Feature] Change Seeders for MVP

--- database/seeders/SpecificationSeeder.php
+++ database/seeders/SpecificationSeeder.php
@@ -13,20 +13,30 @@ class SpecificationSeeder extends Seeder
      */
     public function run(): void
     {
+//        $data = [
+//            ['id' => 1, 'required' => false, 'title' => 'محور', 'type' => SpecificationTypeEnum::SELECT],
+//            ['id' => 2, 'required' => false, 'title' => 'گیربکس', 'type' => SpecificationTypeEnum::SELECT],
+//            ['id' => 3, 'required' => false, 'title' => 'نوع سوخت', 'type' => SpecificationTypeEnum::SELECT],
+//            ['id' => 4, 'required' => false, 'title' => 'کابین', 'type' => SpecificationTypeEnum::SELECT],
+//
+//            ['id' => 5, 'required' => false, 'title' => 'تناژ', 'type' => SpecificationTypeEnum::INPUT_TEXT],
+//            ['id' => 6, 'required' => false, 'title' => 'سابقه تعمیر', 'type' => SpecificationTypeEnum::INPUT_TEXT],
+//            ['id' => 7, 'required' => false, 'title' => 'کیلومتر کارکرد', 'type' => SpecificationTypeEnum::INPUT_TEXT],
+//            ['id' => 8, 'required' => false, 'title' => 'تعداد جابجایی سند', 'type' => SpecificationTypeEnum::INPUT_TEXT],
+//            ['id' => 9, 'required' => false, 'title' => 'سال ساخت', 'type' => SpecificationTypeEnum::INPUT_TEXT],
+//            ['id' => 10, 'required' => false, 'title' => 'وضعیت لاستیک', 'type' => SpecificationTypeEnum::INPUT_TEXT],
+//
+//            ['id' => 11, 'required' => false, 'title' => 'منطقه آزاد', 'type' => SpecificationTypeEnum::SELECT],
+//        ];
+
         $data = [
-            ['id' => 1, 'required' => true, 'title' => 'محور', 'type' => SpecificationTypeEnum::SELECT],
-            ['id' => 2, 'required' => true, 'title' => 'گیربکس', 'type' => SpecificationTypeEnum::SELECT],
-            ['id' => 3, 'required' => true, 'title' => 'نوع سوخت', 'type' => SpecificationTypeEnum::SELECT],
-            ['id' => 4, 'required' => true, 'title' => 'کابین', 'type' => SpecificationTypeEnum::SELECT],
-
-            ['id' => 5, 'required' => true, 'title' => 'تناژ', 'type' => SpecificationTypeEnum::INPUT_TEXT],
-            ['id' => 6, 'required' => true, 'title' => 'سابقه تعمیر', 'type' => SpecificationTypeEnum::INPUT_TEXT],
-            ['id' => 7, 'required' => true, 'title' => 'کیلومتر کارکرد', 'type' => SpecificationTypeEnum::INPUT_TEXT],
-            ['id' => 8, 'required' => true, 'title' => 'تعداد جابجایی سند', 'type' => SpecificationTypeEnum::INPUT_TEXT],
-            ['id' => 9, 'required' => true, 'title' => 'سال ساخت', 'type' => SpecificationTypeEnum::INPUT_TEXT],
-            ['id' => 10, 'required' => true, 'title' => 'وضعیت لاستیک', 'type' => SpecificationTypeEnum::INPUT_TEXT],
-
-            ['id' => 11, 'required' => true, 'title' => 'منطقه آزاد', 'type' => SpecificationTypeEnum::SELECT],
+            ['id' => 1, 'required' => false, 'title' => 'تعداد محور', 'type' => SpecificationTypeEnum::SELECT],
+            ['id' => 2, 'required' => false, 'title' => 'تناژ', 'type' => SpecificationTypeEnum::INPUT_TEXT],
+            ['id' => 3, 'required' => false, 'title' => 'سال تولید', 'type' => SpecificationTypeEnum::INPUT_TEXT],
+            ['id' => 4, 'required' => false, 'title' => 'کارکرد', 'type' => SpecificationTypeEnum::INPUT_TEXT],
+            ['id' => 5, 'required' => false, 'title' => 'گیربکس', 'type' => SpecificationTypeEnum::SELECT],
+            ['id' => 6, 'required' => false, 'title' => 'نوع سوخت', 'type' => SpecificationTypeEnum::SELECT],
+            ['id' => 7, 'required' => false, 'title' => 'تعداد جابه جایی سند', 'type' => SpecificationTypeEnum::INPUT_TEXT],
         ];
 
         DB::table('specifications')->insert($data);
--- database/seeders/SpecificationValueSeeder.php
+++ database/seeders/SpecificationValueSeeder.php
@@ -12,22 +12,35 @@ class SpecificationValueSeeder extends Seeder
      */
     public function run(): void
     {
+//        $data = [
+//            ['id' => 1, 'title' => 'تک محور', 'specification_id' => 1],
+//            ['id' => 2, 'title' => 'جفت محور', 'specification_id' => 1],
+//            ['id' => 3, 'title' => 'سه محور', 'specification_id' => 1],
+//            ['id' => 4, 'title' => 'چهار محور', 'specification_id' => 1],
+//            ['id' => 5, 'title' => 'اتومات', 'specification_id' => 2],
+//            ['id' => 6, 'title' => 'دستی', 'specification_id' => 2],
+//            ['id' => 7, 'title' => 'نیمه اتومات', 'specification_id' => 2],
+//            ['id' => 8, 'title' => 'دیزل', 'specification_id' => 3],
+//            ['id' => 9, 'title' => 'بنزین', 'specification_id' => 3],
+//            ['id' => 10, 'title' => 'cng', 'specification_id' => 3],
+//            ['id' => 11, 'title' => 'برقی', 'specification_id' => 3],
+//            ['id' => 12, 'title' => 'خوابدار', 'specification_id' => 4],
+//            ['id' => 13, 'title' => 'بدون خواب', 'specification_id' => 4],
+//            ['id' => 14, 'title' => 'دارد', 'specification_id' => 11],
+//            ['id' => 15, 'title' => 'ندارد', 'specification_id' => 11],
+//        ];
         $data = [
             ['id' => 1, 'title' => 'تک محور', 'specification_id' => 1],
             ['id' => 2, 'title' => 'جفت محور', 'specification_id' => 1],
             ['id' => 3, 'title' => 'سه محور', 'specification_id' => 1],
             ['id' => 4, 'title' => 'چهار محور', 'specification_id' => 1],
-            ['id' => 5, 'title' => 'اتومات', 'specification_id' => 2],
-            ['id' => 6, 'title' => 'دستی', 'specification_id' => 2],
-            ['id' => 7, 'title' => 'نیمه اتومات', 'specification_id' => 2],
-            ['id' => 8, 'title' => 'دیزل', 'specification_id' => 3],
-            ['id' => 9, 'title' => 'بنزین', 'specification_id' => 3],
-            ['id' => 10, 'title' => 'cng', 'specification_id' => 3],
-            ['id' => 11, 'title' => 'برقی', 'specification_id' => 3],
-            ['id' => 12, 'title' => 'خوابدار', 'specification_id' => 4],
-            ['id' => 13, 'title' => 'بدون خواب', 'specification_id' => 4],
-            ['id' => 14, 'title' => 'دارد', 'specification_id' => 11],
-            ['id' => 15, 'title' => 'ندارد', 'specification_id' => 11],
+            ['id' => 5, 'title' => 'اتومات', 'specification_id' => 5],
+            ['id' => 6, 'title' => 'دستی', 'specification_id' => 5],
+            ['id' => 7, 'title' => 'نیمه اتومات', 'specification_id' => 5],
+            ['id' => 8, 'title' => 'دیزل', 'specification_id' => 6],
+            ['id' => 9, 'title' => 'بنزین', 'specification_id' => 6],
+            ['id' => 10, 'title' => 'cng', 'specification_id' => 6],
+            ['id' => 11, 'title' => 'برقی', 'specification_id' => 6],
         ];
 
         DB::table('specification_values')->insert($data);
