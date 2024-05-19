6e3a8e078b2cc971ba90180db1a404f44fa3008d
Amin Sheykhi <isheykhi@gmail.com>
2024-05-19 17:58:11 +0330

    Create Advertise

--- resources/views/panel/advertise/create.blade.php
+++ resources/views/panel/advertise/create.blade.php
@@ -1,11 +1,6 @@
 <x-layout.admin>
     <x-splade-modal>
-        <x-splade-form :action="route('panel.ad.advertise.store')" :default="[
-           'category_id'=> '4',
-       //    ''=> '',
-       //    ''=> '',
-       //    ''=> '',
-           ]">
+        <x-splade-form :action="route('panel.ad.advertise.store')" :default="[]">
 
             <x-layout.panel.form.card title="Create Advertise">
 
@@ -13,10 +8,7 @@
 
                 <x-splade-textarea label="Description" name="description" class="col-span-full" rows="4"/>
 
-
-
-
-                <x-splade-submit label="Update"/>
+                <x-splade-submit label="Create"/>
 
             </x-layout.panel.form.card>
 
