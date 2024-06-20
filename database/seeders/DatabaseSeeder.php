9ce6a1ebec9b8efe8c15da175c0a9e9e07a172be
Amin Sheykhi <isheykhi@gmail.com>
2024-06-20 14:14:00 +0330

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $directory = 'public/media/ad';
+
+        // Check if directory exists and clear it
+        if (Storage::exists($directory)) {
+            Storage::deleteDirectory($directory);
+            Storage::makeDirectory($directory);
+        }
+
         /* Information on the provinces and cities of Iran */
         $this->call(IranCitiesSeeder::class);
 
@@ -21,11 +30,11 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             AdminUserSeeder::class,
             CategorySeeder::class,
-            SpecificationSeeder::class,
-            ColorSeeder::class,
-            UsageSeeder::class,
-            UsageSpecificationSeeder::class,
-            SpecificationValueSeeder::class,
+//            SpecificationSeeder::class,
+//            ColorSeeder::class,
+//            UsageSeeder::class,
+//            UsageSpecificationSeeder::class,
+//            SpecificationValueSeeder::class,
             // BrandSeeder::class,
             // ProductModelSeeder::class,
             // AdvertiseSeeder::class
