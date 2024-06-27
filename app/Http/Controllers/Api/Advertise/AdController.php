b3587a844f2ed29a747cfab482a13b84abcd29d8
Amin Sheykhi <isheykhi@gmail.com>
2024-06-27 08:42:04 +0330

    Paginate count

--- app/Http/Controllers/Api/Advertise/AdController.php
+++ app/Http/Controllers/Api/Advertise/AdController.php
@@ -16,7 +16,7 @@ class AdController extends Controller
 {
     public function getList()
     {
-        $perPage = request('perPage') ?? 12;
+        $perPage = request('perPage') ?? 24;
         $keyword = request('keyword');
         $categoryIds = request('category_id');
         $provinceIds = request('province_id');
