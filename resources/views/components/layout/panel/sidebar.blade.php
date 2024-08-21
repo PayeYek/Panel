7ce8681fea05f44e21a619e7a5ebde455cdbf453
Amin Sheykhi <isheykhi@gmail.com>
2024-08-21 16:46:12 +0330

    Article

--- resources/views/components/layout/panel/sidebar.blade.php
+++ resources/views/components/layout/panel/sidebar.blade.php
@@ -35,7 +35,7 @@
         </ul>
 
         <ul>
-            <x-layout.panel.menu title="Sell" route="panel.blog.*">
+            <x-layout.panel.menu title="Blog" route="panel.blog.*">
             <x-layout.panel.menu title="Articles" route="panel.blog.article.index"/>
             <x-layout.panel.menu title="Companies" route="panel.blog.company.index"/>
             <x-layout.panel.menu title="Files" route="panel.blog.file.index"/>
