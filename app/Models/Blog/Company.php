ba0dd3153bc54f834b1140b18bdd671178556aa6
themestermim <themestermim@gmail.com>
2024-08-26 16:49:12 +0330

    add id

--- app/Models/Blog/Company.php
+++ app/Models/Blog/Company.php
@@ -13,7 +13,7 @@ class Company extends Model
     use HasFactory;
 
     protected $table = 'companies';
-    protected $fillable = ['title', 'slug', 'logo', 'description', 'body'];
+    protected $fillable = ['id', 'title', 'slug', 'logo', 'description', 'body'];
 
     public function getLogoAttribute()
     {
