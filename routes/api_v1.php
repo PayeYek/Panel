8f85cd6f42dd091387bf8bdcbc004241a46d4440
themestermim <themestermim@gmail.com>
2024-08-27 16:32:50 +0330

    param added.

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -163,4 +163,4 @@ Route::prefix('data')->name('data.')->group(function () {
 
 
 Route::get('/article_companies', [ArticleController::class, 'getAllCompanies']);
-Route::get('/related_article', [ArticleController::class, 'relatedArticles']);
+Route::get('/related_article/{articleId}', [ArticleController::class, 'relatedArticles']);
